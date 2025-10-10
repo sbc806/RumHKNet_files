bfeebfe6c420154e18dd1ea73bdd4bf0fbb46fcf
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-10 14:42:49 -0700

    Update generate_weights.py

--- multiclass_weights/generate_weights.py
+++ multiclass_weights/generate_weights.py
@@ -8,5 +8,8 @@ for i in range(0,133):
     total=len(train_multiclass)
     count=train_multiclass["label"].value_counts()[i]
     print(i,total,count)
-    print(total/133/count,np.floor(total/133/count))
+    new_weight=None
+    if np.floor(total/133/count) == 0:
+        new_weight=1/5
+    print(total/133/count,np.floor(total/133/count),new_weight)
     print()
