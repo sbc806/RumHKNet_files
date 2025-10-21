80935fcbfba76465af5656e40ab66185b254792d
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-20 19:44:47 -0700

    Create check_overlap.sh

---
+++ data_splits/check_overlap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --cpus-per-node=1
+#SBATCH --mem=64G
+#SBATCH --time=7-0
+#SBATCH --job-name=check-overlap
+#SBATCH --output=output/check_overlap_%j.out
+#SBATCH --err=output/check_overlap_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+python -u check_overlap.py
+
+
+deactivate
