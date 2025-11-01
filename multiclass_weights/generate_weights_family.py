e546372b8caeb69e5cc735623333dfd5a9c5ae70
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-31 22:39:05 -0700

    Update generate_weights_family.py

--- multiclass_weights/generate_weights_family.py
+++ multiclass_weights/generate_weights_family.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-train_multiclass = pd.read_csv("/home/schen123/projects/def-guanuofa/schen123/kinases/kinases_dataset/step_3_family_filtered/train/train.csv")
+train_multiclass = pd.read_csv("/home/schen123/projects/def-guanuofa/schen123/kinases/kinases_dataset/step_3_family_filtered/protein/multi_class/train/train.csv")
 print(train_multiclass["label"].value_counts())
 for i in range(0,10):
     total=len(train_multiclass)
