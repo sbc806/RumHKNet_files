f620d3f19e944307bb917ed088afad90e51793e1
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-07 16:23:02 -0800

    Update step_3_family_filtered_subword.sh

--- create_subword_vocab/step_3_family_filtered_subword.sh
+++ create_subword_vocab/step_3_family_filtered_subword.sh
@@ -16,12 +16,12 @@ source TEST/bin/activate
 
 cd ../sbc806/LucaPCycle/src/data_process/V3
 
-python rumhknet_subword_step_3_family_filtered.py --func corpus --outfile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus.txt
+python rumhknet_subword_step_3_family_filtered.py --func corpus --outfile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus.txt
 
-python rumhknet_subword_step_3_family_filtered.py --func learn_bpe --num_symbols=30000 --infile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus.txt --outfile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus_codes_30000.txt --verbose
+python rumhknet_subword_step_3_family_filtered.py --func learn_bpe --num_symbols=30000 --infile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus.txt --outfile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus_codes_30000.txt --verbose
 
-python rumhknet_subword_step_3_family_filtered.py --func apply_bpe --infile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus.txt --codes_file ../../subword/step_3_family_filtered/step_3_all_sequences_corpus_codes_30000.txt --outfile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus_token_30000.txt
+python rumhknet_subword_step_3_family_filtered.py --func apply_bpe --infile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus.txt --codes_file ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus_codes_30000.txt --outfile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus_token_30000.txt
 
-python rumhknet_subword_step_3_family_filtered.py --func get_vocab --infile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus_token_30000.txt --outfile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus_subword_vocab_30000_ori.txt
+python rumhknet_subword_step_3_family_filtered.py --func get_vocab --infile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus_token_30000.txt --outfile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus_subword_vocab_30000_ori.txt
 
-python rumhknet_subword_step_3_family_filtered.py --func subword_vocab_2_token_vocab --infile ../../subword/step_3_family_filtered/step_3_all_sequences_corpus_subword_vocab_30000_ori.txt --outfile ../../vocab/step_3_family_filtered/step_3_all_sequences_corpus_subword_vocab_30000.txt
+python rumhknet_subword_step_3_family_filtered.py --func subword_vocab_2_token_vocab --infile ../../../../../subword/step_3_family_filtered/step_3_all_sequences_corpus_subword_vocab_30000_ori.txt --outfile ../../../../../vocab/step_3_family_filtered/step_3_all_sequences_corpus_subword_vocab_30000.txt
