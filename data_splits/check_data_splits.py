918e7ed01530708395067a294975828b8bb46251
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-12 15:22:19 -0800

    Update check_data_splits.py

--- data_splits/check_data_splits.py
+++ data_splits/check_data_splits.py
@@ -1,6 +1,9 @@
 import argparse
+import os
+import pandas as pd
+import numpy as np
 
-parser = argparse.ArgumentPrarser()
+parser = argparse.ArgumentParser()
 parser.add_argument("dir_path", type=str, required=True)
 
 dir_path = args.dir_path
@@ -10,7 +13,7 @@ test_path = os.path.join(test_path, "test/test.csv")
 
 train_df = pd.read_csv(train_path)
 dev_df = pd.read_csv(dev_path)
-test_path = pd.read_csv(test_path)
+test_df = pd.read_csv(test_path)
 
 full_df = pd.concat((train_df, dev_df, test_df), axis=0)
 
@@ -18,9 +21,13 @@ print("Total number of sequences:", len(full_df))
 print("Number of unique sequences:", np.unique(full_df["seq"]).shape)
 
 split_dfs = [train_df, dev_df, test_df]
+split_names = ["train", "dev", "test"]
 for i, each_df in enumerate(split_dfs):
   print(f"Number of samples in {split_names[i]}: {len(each_df)}")
   labels = each_df["label"]
   print(f"Labels for {split_names[i]}: {np.unique(labels)}")
+  if "batch" in each_df.columns:
+    batches = each_df["batch"]
+    print(f"Batches for {split_names[i]}: {np.unique(batches)}")
 
 
