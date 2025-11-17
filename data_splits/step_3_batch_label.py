de7a7ef99d315b0a4e49afe95cb810a64d686a47
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 09:54:20 -0800

    Update step_3_batch_label.py

--- data_splits/step_3_batch_label.py
+++ data_splits/step_3_batch_label.py
@@ -2,14 +2,16 @@ import os
 import pandas as pd
 import numpy as np
 
-dir_path="../../kinases_dataset/extra_p_133_class_v3"
-with open(os.path.join(dir_path,"label.jsn","r")) as f:
+dir_path="../../kinases_dataset/extra_p_133_class_v3/protein/multi_class"
+with open(os.path.join(dir_path,"label.json","r")) as f:
   ko_category_label=json.load(f)
 label_ko_category={}
 for ko_category in ko_category_label:
   label_ko_category[ko_category_label[ko_category]]=ko_category
+with open(os.path.join("../../kinases_dataset/step_3_family_filtered/protein/multi_class/protein/multi_class/ko_category_label.json","r")) as f:
+  ko_category_family=json.load(f)
 train_df=pd.read_csv(os.path.join(dir_path,"train/train.csv"))
-dev_df=pd.read_csv(os.pat.join(dir_path,"dev/dev.csv"))
+dev_df=pd.read_csv(os.path.join(dir_path,"dev/dev.csv"))
 test_df=pd.read_csv(os.path.join(dir_path,"test/test.csv"))
 
 dfs=[train_df,dev_df,test_df]
