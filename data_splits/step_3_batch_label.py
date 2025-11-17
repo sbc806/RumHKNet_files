d4a42e7d2bf315a4337dbb12e488694392b27e76
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 10:00:03 -0800

    Update step_3_batch_label.py

--- data_splits/step_3_batch_label.py
+++ data_splits/step_3_batch_label.py
@@ -19,11 +19,11 @@ dfs=[train_df,dev_df,test_df]
 for df in dfs:
   for i in range(0,len(df)):
     class_label=df["label"].iloc[i]
-    ko_category=label_ko_category[class_label]
+    ko_category=label_ko_category[str(class_label)]
     if ko_category in ko_category_family:
       family_label=ko_category_family[ko_category]
     else:
-      family_label="10"
+      family_label=10
     df["label"].iloc[i]=family_label
   print(df)
   print(np.unique(df["label"]))
