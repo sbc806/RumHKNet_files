7a93b8b087f43645e85c077a347a1f2ce2f9475e
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 10:28:09 -0800

    Update generate_weights_11_family.py

--- multiclass_weights/generate_weights_11_family.py
+++ multiclass_weights/generate_weights_11_family.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 
 
-train_multiclass = pd.read_csv("/home/schen123/projects/def-guanuofa/schen123/kinases/kinases_dataset/step_3_family_filtered/protein/multi_class/train/train.csv")
+train_multiclass = pd.read_csv("/home/schen123/projects/def-guanuofa/schen123/kinases/kinases_dataset/step_3_11_family/protein/multi_class/train/train.csv")
 print(train_multiclass["label"].value_counts())
-for i in range(0,10):
+for i in range(0,11):
     total=len(train_multiclass)
     count=train_multiclass["label"].value_counts()[i]
     print(i,total,count)
     new_weight=None
     if np.floor(total/10/count) == 0:
         new_weight=1/5
-    print(total/10/count,np.floor(total/10/count),new_weight,total/(10*count))
+    print(total/11/count,np.floor(total/11/count),new_weight,total/(11*count))
     print()
