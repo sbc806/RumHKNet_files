6990d242bf67e3d10d66f9c9cca348f20500fa3e
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 09:51:13 -0800

    Update step_3_batch_label.py

--- data_splits/step_3_batch_label.py
+++ data_splits/step_3_batch_label.py
@@ -13,4 +13,12 @@ dev_df=pd.read_csv(os.pat.join(dir_path,"dev/dev.csv"))
 test_df=pd.read_csv(os.path.join(dir_path,"test/test.csv"))
 
 dfs=[train_df,dev_df,test_df]
-
+for df in dfs:
+  for i in range(0,len(df)):
+    class_label=df["label"].iloc[i]
+    ko_category=label_ko_category[class_label]
+    if ko_category in ko_category_family:
+      family_label=ko_category_family[ko_category]
+    else:
+      family_label="10"
+    df["label"].iloc[i]=family_label
