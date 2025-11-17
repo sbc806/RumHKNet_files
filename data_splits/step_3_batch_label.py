ca38b0c1ce6ad1ccca8582258332339b6b6a3fa3
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 09:48:48 -0800

    Update step_3_batch_label.py

--- data_splits/step_3_batch_label.py
+++ data_splits/step_3_batch_label.py
@@ -1,1 +1,16 @@
+import os
+import pandas as pd
+import numpy as np
+
+dir_path="../../kinases_dataset/extra_p_133_class_v3"
+with open(os.path.join(dir_path,"label.jsn","r")) as f:
+  ko_category_label=json.load(f)
+label_ko_category={}
+for ko_category in ko_category_label:
+  label_ko_category[ko_category_label[ko_category]]=ko_category
+train_df=pd.read_csv(os.path.join(dir_path,"train/train.csv"))
+dev_df=pd.read_csv(os.pat.join(dir_path,"dev/dev.csv"))
+test_df=pd.read_csv(os.path.join(dir_path,"test/test.csv"))
+
+dfs=[train_df,dev_df,test_df]
 
