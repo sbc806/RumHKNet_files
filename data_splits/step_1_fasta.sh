a05f6681700b13fdddb3717f1d14a8b1c3624140
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-24 16:59:19 -0800

    Update step_1_fasta.sh

--- data_splits/step_1_fasta.sh
+++ data_splits/step_1_fasta.sh
@@ -1,3 +1,3 @@
-python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/train/train.csv --fasta_dir_path ../../kinases_dataset/prediction/step_1 --num_fasta 3
-python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/dev/dev.csv --fasta_dir_path ../../kinases_dataset/prediction/step_1 --num_fasta 1
-python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/test/test.csv --fasta_dir_path ../../kinases_dataset/prediction/step_1 --num_fasta 1
+python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_class/train/train.csv --fasta_dir_path ../../kinases_dataset/prediction/step_1 --num_fasta 3
+python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_classdev/dev.csv --fasta_dir_path ../../kinases_dataset/prediction/step_1 --num_fasta 1
+python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_class/test/test.csv --fasta_dir_path ../../kinases_dataset/prediction/step_1 --num_fasta 1
