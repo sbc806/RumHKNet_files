51d366324a4e3d1734e9fe7f7d7832c752c58a4b
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-24 16:35:03 -0800

    Update convert_data_splits_fasta_individual.py

--- data_splits/convert_data_splits_fasta_individual.py
+++ data_splits/convert_data_splits_fasta_individual.py
@@ -13,16 +13,19 @@ df = pd.read_csv(args.csv_path)
 
 print("Total number of sequences:", len(df))
 print("Number of unique sequences:", np.unique(df["seq"]).shape)
-
+split_name = args.csv_path.split("/")[-1].split(".csv")[0]
 fasta_dir_path = args.fasta_dir_path
 all_dfs = []
+extension = ""
 for i in range(0, args.num_fasta):
   split_size = len(df) // args.num_fasta
   split_df = df[i*split_size: i*split_size+split_size]
+  if args.num_fasta > 1:
+      extension = f"_{i}"
   for j in range(0, len(split_df)):
-    with open(os.path.join(fasta_dir_path, f'{split_name}_{extension}.fasta'), 'w') as f:
+    with open(os.path.join(fasta_dir_path, f'{split_name}{extension}.fasta'), 'w') as f:
       f.write(">"+split_df["seq_id"].iloc[j]+"\n")
-      f.write(split_df["seq"].iloc[j})
+      f.write(split_df["seq"].iloc[j])
       if j < len(split_df)-1:
         f.write("\n")
 
@@ -30,10 +33,10 @@ full_df = pd.concat(all_dfs, axis=0)
 print("Total number of sequences:", len(full_df))
 print("Number of unique sequences:, np.unique(full_df["seq"]).shape)
 
-small_d f = df[0:5]
+small_df = df[0:5]
 for i in range(0, len(small_df)):
   with open(os.path.join(fasta_dir_path, f'{split_name}_five_sequences.fasta'), 'w') as f:
-    f.write("?"+small_df["seq_id"].iloc[i]+"\n")
+    f.write(">"+small_df["seq_id"].iloc[i]+"\n")
     f.write(small_df["seq"].iloc[i])
-    if i <len(split_df)-1:
-    f.write("\n")
+    if i < len(split_df)-1:
+      f.write("\n")
