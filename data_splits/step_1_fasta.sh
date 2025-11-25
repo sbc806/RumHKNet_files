37c1b0c9f5f09e778dcd6fc5b015cad561f9755e
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-24 16:54:06 -0800

    Update step_1_fasta.sh

--- data_splits/step_1_fasta.sh
+++ data_splits/step_1_fasta.sh
@@ -1,1 +1,1 @@
-
+python convert_data_splits_fasta_individual.py --csv_path ../../kinases_dataset/step_1_non_kinases_preprocessed/train/train.csbv --fasta_path ../../kinases_dataset/prediction/step_1 --n_fasta 3
