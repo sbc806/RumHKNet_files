fcf40e092a8b5327fae6fe9e9accc0ac40aaa1e7
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-28 07:54:01 -0800

    Update step_1_evaluate_threshold.py

--- step_1_evaluate_threshold.py
+++ step_1_evaluate_threshold.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os as os
 import pandas as pd
 
@@ -5,10 +6,10 @@ dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 predictions_dir=os.path.join(dir_path,"predictions/predicted_results/step_1")
 prediction_csv=os.lsitdir(predictions_dir)
 prediction_csv_filtered=[ f for f in prediction_csv if "five_sequences" not in f]
-
+threshold_files={}
 for f in prediction_csv_filtered:
-  tehreshold=prediction_csv_filtered.split(".csv")[0].split("_")[-1]
-  if threshold int rehshold_files:
+  threshold=prediction_csv_filtered.split(".csv")[0].split("_")[-1]
+  if threshold in threshold_files:
     threshold_files[threshold].append(f)
   else:
     threshold_files[threshold]=[f]
@@ -22,3 +23,10 @@ def stack_csvs(files):
     df=pd.read_csv(f)
     dfs.append(df)
   return pd.concat(dfs)
+df_07=stack_csvs(threshold_files["07"])
+
+split_dir_path=os.path.join(dir_path,"sbc806/RumHKNet/kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_class")
+train_path=os.path.join(split_dir_path,"train/train.csv")
+train_df=pd.read_csv(train_path)
+print("Number of xamples in training set:",len(train_df))
+print("Number of common rows:",np.sum(train_df["seq_id"]==df_07["seq_id"]]))
