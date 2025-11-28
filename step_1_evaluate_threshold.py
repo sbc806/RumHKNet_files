4abef8011228c661ce77ba9f78997cbd5811efa5
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-28 07:50:27 -0800

    Update step_1_evaluate_threshold.py

--- step_1_evaluate_threshold.py
+++ step_1_evaluate_threshold.py
@@ -1,10 +1,10 @@
 import os as os
 import pandas as pd
 
-dir="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
+dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 predictions_dir=os.path.join(dir_path,"predictions/predicted_results/step_1")
 prediction_csv=os.lsitdir(predictions_dir)
-predition_csv=[ f for f in prediction_csv if "five_sequences " not in f]
+prediction_csv_filtered=[ f for f in prediction_csv if "five_sequences" not in f]
 
 for f in prediction_csv_filtered:
   tehreshold=prediction_csv_filtered.split(".csv")[0].split("_")[-1]
