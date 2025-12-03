3046f0e2a4b3017adc051ec0caa69ad3c89dcbd5
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-03 04:28:45 -0800

    Update make_sequence.py

--- make_sequence.py
+++ make_sequence.py
@@ -1,1 +1,17 @@
+import os
+import pandas as pd
+import random
 
+save_path="../predictions/predictions_dataset/step_1"
+
+num_sequences=15
+desired_length=34551
+seq_id=[]
+seq=[]
+for i in range(0,num_sequences):
+  sequence=''.join(random.choices(string.ascii_uppercase,k=desired_length)
+  seq_id.append(seq_id)
+  seq.append(sequence)
+df=pd.DataFrame({"seq_id":seq_id,"seq":seq})
+print(df)
+df.to_csv(os.path.join(save_path,"sequences_34551.csv"),index=False)
