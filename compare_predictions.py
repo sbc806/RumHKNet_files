91c11d6a8105f5ebd2274b8bd9e12603664a4760
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-02 21:20:59 -0800

    Update compare_predictions.py

--- compare_predictions.py
+++ compare_predictions.py
@@ -2,17 +2,20 @@ import os
 import pandas as pd
 
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both"
-result_names=["train_0_predicted_03.csv","train_0_10000_new_predicted_03_3432_v3.csv","train_0_100000_new_predicted_03_3432_v3.csv"]
+result_names=["train_0_predicted_02.csv","train_0_10000_new_predicted_03_3432_v3.csv","train_0_100000_new_predicted_03_3432_v3.csv"]
 dfs=[]
 for f_name in result_names:
   dfs.append(pd.read_csv(os.path.join(dir_path,f_name)))
-
+new_preds=dfs[0]["prob"]<=0.3
 for i in range(0,10000):
   seq_id=dfs[0]["seq_id"].iloc[i]
   probs=[]
   preds=[]
   for j in range(0,len(dfs)):
     probs.append(dfs[j]["prob"].iloc[i])
-    preds.append(dfs[j]["pred"].iloc[i])
+    if j==0:
+      preds.append(new_preds.iloc[i]
+    else:
+      preds.append(dfs[j]["pred"].iloc[i])
   print(seq_id,probs,preds)
   print()
