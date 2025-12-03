fef5c477e16b067a62022018b3c38b0ccdfc2b58
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-03 04:37:48 -0800

    Update make_sequence.py

--- make_sequence.py
+++ make_sequence.py
@@ -11,7 +11,7 @@ seq_id=[]
 seq=[]
 for i in range(0,num_sequences):
   sequence=''.join(random.choices(string.ascii_uppercase,k=desired_length))
-  seq_id.append(seq_id)
+  seq_id.append("fake_seq_"+str(i))
   seq.append(sequence)
 df=pd.DataFrame({"seq_id":seq_id,"seq":seq})
 print(df)
