f99f8d29f5a050ec1116e789410a733839586788
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-02 21:19:27 -0800

    Update compare_predictions.py

--- compare_predictions.py
+++ compare_predictions.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results"
+dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both"
 result_names=["train_0_predicted_02.csv","train_0_10000_new_predicted_03_3432_v3.csv","train_0_100000_new_predicted_03_3432_v3.csv"]
 dfs=[]
 for f_name in result_names:
