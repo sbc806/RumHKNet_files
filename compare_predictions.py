61df819da09f71039b255a4cfb38ca4401ea3bab
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-02 21:18:46 -0800

    Update compare_predictions.py

--- compare_predictions.py
+++ compare_predictions.py
@@ -4,8 +4,8 @@ import pandas as pd
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results"
 result_names=["train_0_predicted_02.csv","train_0_10000_new_predicted_03_3432_v3.csv","train_0_100000_new_predicted_03_3432_v3.csv"]
 dfs=[]
-for name in result_names:
-  dfs.append(pd.read_csv(os.path.join(dir_path,f_name))
+for f_name in result_names:
+  dfs.append(pd.read_csv(os.path.join(dir_path,f_name)))
 
 for i in range(0,10000):
   seq_id=dfs[0]["seq_id"].iloc[i]
