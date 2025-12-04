a7b984a58b2c71a20a9bf434033b9b1fd089eca5
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-03 16:20:10 -0800

    Update evaluate_length.py

--- evaluate_length.py
+++ evaluate_length.py
@@ -92,7 +92,7 @@ other_kinase_file = "Final_other_kinase_760926.fasta"
 
 # all_data = {"Histidine_kinase": sequence_data}
 # sequence_df=pd.DataFrame(histidine_data)
-total_sequences=pd.DataFrame("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/train_0_100000.csv")
+sequence_df=pd.read_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/train_0_100000.csv")
 total_sequences=len(sequence_df)
 print("Total number of sequences:", total_sequences)
 print("Minimum sequence length:", np.min(sequence_df["seq_length"]))
