b73612d32b0a4119854a82b444c130493b1556b9
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-04 23:47:13 -0800

    Update read_chunk.py

--- read_chunk.py
+++ read_chunk.py
@@ -1,7 +1,7 @@
 import pandas
 
 count = 0
-with pd.read_csv("../predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95_small_0.csv,chunksize=50) as csv_reader:
+with pd.read_csv("../predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95_small_0.csv",chunksize=50) as csv_reader:
   for chunk in csv_reader:
     count=count+1
 print(count)
