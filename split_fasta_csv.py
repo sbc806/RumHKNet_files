1e061a2034e7b9202eec831e85cb67c0deefcf47
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-04 22:34:16 -0800

    Update split_fasta_csv.py

--- split_fasta_csv.py
+++ split_fasta_csv.py
@@ -16,9 +16,9 @@ all_dfs=[]
 for i in range(0,total_length):
   start=i*split_size
   end=start+split_size
-  selected_df=df_small_seq
+  selected_df=df_small_seq[start:end]
   if i == total_length-1:
-    selected_df=df_small_seq
+    selected_df=df_small_seq[start:]
   all_dfs.append(selected_df)
   selected_df.to_csv(f"../clustered_rep_seq95_small_{i}.csv",index=False)
 df_small_seq_1=pd.concat(all_dfs)
