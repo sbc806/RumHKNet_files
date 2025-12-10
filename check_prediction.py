9c6e5370822356d1385bf7c9f05aaf280cd39b49
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-09 20:47:21 -0800

    Update check_prediction.py

--- check_prediction.py
+++ check_prediction.py
@@ -1,1 +1,14 @@
+import os
+import pandas as pd
 
+
+predictions_path="../predictions/predicted_results/step_1/both/clustered"
+
+def get_predictions_df(predictions_path,i):
+  prediction_files=os.listdi(predictions_path)
+  selected_files=[f for f in prediction_files if "small_"+str(i) in f]
+  selected_files=sorted(selected_files)
+  print(selected_files)
+  df=None
+  for f in selected_files:
+    current_df=pd.read_csv(os.path.join(predictions_path,f))
