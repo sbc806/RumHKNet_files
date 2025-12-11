1c838fb364d950f89bcc82e24a3fd1536ba525da
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-11 07:56:40 -0800

    Update split_large.py

--- split_large.py
+++ split_large.py
@@ -5,7 +5,7 @@ import os
 large_path="../predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95_large.csv"
 large_df=pd.read_csv(large_path)
 large_sequence_length=large_df["seq"].str.len()
-large_df_sorted=large_df[np.argsort(large_sequence_length)]
+large_df_sorted=large_df.iloc[np.argsort(large_sequence_length)]
 print(large_df_sorted)
 print(large_df_sorted["seq"].str.len())
 large_df_sorted_subset=large_df_sorted.iloc[-1250:]
