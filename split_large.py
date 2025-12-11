0dd8110f5dc5365bd46bfdbdee5667d3d7236df3
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-11 07:55:31 -0800

    Update split_large.py

--- split_large.py
+++ split_large.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-large_path="../predictions/predictions_dataset/step_1/clustered_rep_seq95_large.csv"
+large_path="../predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95_large.csv"
 large_df=pd.read_csv(large_path)
 large_sequence_length=large_df["seq"].str.len()
 large_df_sorted=large_df[np.argsort(large_sequence_length)]
@@ -10,4 +10,4 @@ print(large_df_sorted["seq"].str.len())
 large_df_sorted_subset=large_df_sorted.iloc[-1250:]
 print(large_df_sorted_subset)
 print(large_df_sorted_subset.str.len())
-large_df_sorted_subset.to_csv("../predictions/predictions_dataset/step_1/clustered_rep_seq95_large_last_1250.csv",index=False)
+large_df_sorted_subset.to_csv("../predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95_large_last_1250.csv",index=False)
