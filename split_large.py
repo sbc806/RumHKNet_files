23a3396f29b3f387657c6f188e940ffad1c38d50
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-11 08:26:55 -0800

    Update split_large.py

--- split_large.py
+++ split_large.py
@@ -12,5 +12,5 @@ large_df_sorted_subset=large_df_sorted.iloc[-1250:]
 print(large_df_sorted_subset)
 print(large_df_sorted_subset["seq"].str.len())
 large_df_sorted_subset.to_csv("../predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95_large_sorted_last_1250.csv",index=False)
-header_df=pd.DataFrame({"seq":[],"seq_id":[],"prob":[],"pred":[]})
+header_df=pd.DataFrame({"seq":[],"seq_id":[],"prob":[],"label":[]})
 header_df.to_csv("../predictions/predicted_results/step_1/both/clustered/clustered_rep_seq95_large_sorted_last_1250_predicted_03.csv",index=False)
