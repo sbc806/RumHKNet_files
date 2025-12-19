a287999b4091cddd0f4dc1c173d13a2fb74e21ae
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-18 16:49:19 -0800

    Update check_predictions_large.py

--- check_predictions_large.py
+++ check_predictions_large.py
@@ -5,7 +5,7 @@ import pandas as pd
 predictions_dataset_dir_path="../predictions/predictions_dataset/step_1/clustered"
 predicted_results_dir_path="../predictions/predicted_results/step_1/both/clustered"
 
-large_df=os.path.join(predictions_dataset_dir_path,"clustered_rep_seq95_large.csv")
+large_df=pd.read_csv(os.path.join(predictions_dataset_dir_path,"clustered_rep_seq95_large.csv"))
 print("Number of sequences with length > 1500:",len(large_df))
 print("Number of unique seq_id:",np.unique(large_df["seq_id"]).shape)
 print("Number of unique seq:",np.unique(large_df["seq"]).shape)
