5d907e8fa96c627bbbb6cfff3fb73080a8d83321
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-27 23:08:30 -0800

    Update newrun_fasta.py

--- newrun_fasta.py
+++ newrun_fasta.py
@@ -1,3 +1,14 @@
 import os as os
 import pandas as pd
-import Bio
+from Bio import SeqIoO
+
+fasta_content=SeqIO.patse(open("../newrun_seqs.fasta"),'fasta')
+seq_records=[]
+for i, fasta in enumerate(fasta_content):
+  name,sequence=fasta.id,str(fasta.seq)
+  seq_record={"seq_id":name,"seq":sequence}
+  seq_records.append(seq_record)
+
+df=pd.DataFrame(seq_records)
+print(df)
+df.to_csv("../predictions/predictions_dataset/step_1/clustered/newrun_seqs.csv",index=False)
