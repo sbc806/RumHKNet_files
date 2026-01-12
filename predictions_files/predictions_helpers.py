2b9975710f8f00bc5f041bec741ae485209c910f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-11 20:50:01 -0800

    Update predictions_helpers.py

---
+++ predictions_files/predictions_helpers.py
@@ -0,0 +1,12 @@
+import os as os
+import pandas as pd
+
+def predictions_information(df):
+  seq_id=df["seq_id"]
+  seq=df["seq"]
+  unique_seq_id=np.unique(seq_id)
+  unique_seq=np.unique(seq)
+  print("Number of seq_id:",len(seq_id))
+  print("Number of seq:",len(seq))
+  print("Number of unique seq_id:",unique_seq_id.shape)
+  print("Number of unique seq:",unique_seq.shape)
