336b12d409587d8f63a555f3d100ffa945dfe300
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-12 10:19:59 -0800

    Update step_3_results.py

--- predictions_files/step_3_results.py
+++ predictions_files/step_3_results.py
@@ -21,7 +21,7 @@ predictions_information(small_histidine_df)
 large_histidine_df=pd.read_csv(os.path.join(predictions_path,"clustered_rep_seq95_large_histidine_kinase_predicted_03.csv"))
 predictions_information(large_histidine_df)
 
-with open("../../sbc806/RumHKNet/kinases_dataset/step_11_family/protein/multi_class/label.json","r") as f:
+with open("../../sbc806/RumHKNet/kinases_dataset/step_3_11_family/protein/multi_class/label.json","r") as f:
   other_label=json.load(f)
 small_histidine_df_new=add_label(small_histidine_df,other_label)
 
@@ -30,6 +30,7 @@ large_histidine_df_new=add_label(large_histidine_df,other_label)
 print(np.unique(pd.read_csv("../../sbc806/RumHKNet/kinases_dataset/extra_p_133_class_v3_batch/protein/multi_class/train/train.csv")))
 print(np.unique(pd.read_csv("../../sbc806/RumHKNet/kinases_dataset/extra_p_133_class_v3_batch/protein/multi_class/dev/dev.csv")))
 print(np.unique(pd.read_csv("../../sbc806/RumHKNet/kinase_dataset/extra_p_133_class_v3_batch/protein/multi_class/test/test.csv")))
+
 
 
 
