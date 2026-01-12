7f2bb61812e40884f9df06b35130e2d7be0e4e19
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-11 20:46:26 -0800

    Update step_1_datasets.py

--- predictions_files/step_1_datasets.py
+++ predictions_files/step_1_datasets.py
@@ -1,1 +1,10 @@
+import numpy as np
+import os as np
 import pandas as pd
+from predictions_helpers import predictions_information
+
+predictions_dataset_path="../../predictions/predictions_dataset/step_1/clustered"
+clustered_df=pd.read_csv(os.path.join(predictions_dataset_path,"clustered_rep_seq95.csv"))
+new_seqs_df=pd.read_csv(os.path.join(predictions_dataset_path,"newrun_seqs.csv"))
+predictions_information(clustered_df)
+predictions_information(new_seqs_df)
