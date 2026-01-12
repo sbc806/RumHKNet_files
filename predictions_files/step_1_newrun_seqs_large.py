9c42ccf498115a1e84c7471e05f4536134ed57ff
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-12 09:09:27 -0800

    Update step_1_newrun_seqs_large.py

--- predictions_files/step_1_newrun_seqs_large.py
+++ predictions_files/step_1_newrun_seqs_large.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os as os
 
+dataset_path="../../predictions/predictions_dataset/step_1/clustered"
 predictions_path="../../predictions/predicted_results/step_1/both/clustered"
 df_1=pd.read_csv(os.path.join(predictions_path,"newrun_seqs_large_0_predicted_03.csv"))
 df_2=pd.read_csv(os.path.join(predictions_path,"newrun_seqs_large_1_predicted_03.csv"))
@@ -10,3 +11,6 @@ print(np.unique(df["seq"]).shape)
 print(np.unique(df["seq_id"]).shape)
 length=df["seq"].str.len()>=12211
 print(np.sum(length))
+
+df_12211=df[length].iloc[:,0:2]
+df_12211.to_csv(os.path.join(dataset_path,"newrun_seqs_large_12211.csv"),index=False)
