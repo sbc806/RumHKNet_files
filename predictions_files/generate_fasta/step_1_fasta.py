eb2ad38387d5a905327cd79f3e2363ced70f880b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-14 18:23:21 -0800

    Update step_1_fasta.py

--- predictions_files/generate_fasta/step_1_fasta.py
+++ predictions_files/generate_fasta/step_1_fasta.py
@@ -3,8 +3,8 @@ import os
 import pandas as pd
 from predictions_helpers import predictions_information
 
-dataset_path="../../predictions/predictions_dataset/step_1/clustered"
-predictions_path="../../predictions/predicted_results/step_1/both/clustered"
+dataset_path="../../../predictions/predictions_dataset/step_1/clustered"
+predictions_path="../../../predictions/predicted_results/step_1/both/clustered"
 
 def get_predictions_df(predictions_path,i):
   prediction_files=os.listdir(predictions_path)
@@ -179,8 +179,9 @@ clustered_rep_seq95_kinase_df=clustered_rep_seq95_all_df[clustered_rep_seq95_all
 newrun_seqs_kinase_df=newrun_seqs_all_df[newrun_seqs_all_df.iloc[:,3]==1]
 step_1_kinase_df=pd.concat([clustered_rep_seq95_kinase_df,newrun_seqs_kinase_df])
 predictions_information(step_1_kinase_df)
-step_1_fasta_path=os.path.join(predictions_path,"step_1_kinase_clustered_newrun.fasta")
+step_1_fasta_path=os.path.join("../../../RumHKNet_fasta/step_1_kinase_clustered_newrun.fasta")
 # df_to_fasta(step_1_kinase_df,step_1_fasta_path)
+
 
 
 
