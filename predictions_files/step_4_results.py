9158e1f8d311f55741b4dddd292abd1609fc5cd2
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-15 16:42:12 -0800

    Update step_4_results.py

--- predictions_files/step_4_results.py
+++ predictions_files/step_4_results.py
@@ -11,7 +11,7 @@ for f in os.listdir(small_dir_0_path):
   df=pd.read_csv(os.path.join(small_dir_0_path,f))
   small_dfs.append(df)
   
-small_df=pd.concat(small_dfs)
+complete_small_df=pd.concat(small_dfs)
 predictions_information(complete_small_df)
 
 complete_large_df=pd.read_csv(os.path.join(predictions_path,"clustered_rep_seq95_large_histidine_kinase_batch_predicted_03.csv"))
@@ -20,4 +20,5 @@ predictions_information(complete_large_df)
 print("newrun_seqs")
 complete_large_df=pd.read_csv(os.path.join(predictions_path,"newrun_seqs_large_histidine_kinase_batch_predicted_03.csv"))
 predictions_information(complete_large_df)
+
 
