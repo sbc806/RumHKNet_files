f393c556f959a0ee584ba5cd228dc6383cbd6053
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-15 23:21:02 -0800

    Update step_3_fasta.py

--- predictions_files/generate_fasta/step_3_fasta.py
+++ predictions_files/generate_fasta/step_3_fasta.py
@@ -158,7 +158,8 @@ predictions_information(step_3_histidine_df)
 print(np.unique(step_3_histidine_df.iloc[:,2]))
 print(np.unique(step_3_histidine_df.iloc[:,3]))
 step_3_fasta_path="../../../RumHKNet_fasta/step_3_kinase_family_clustered_newrun.fasta"
-# df_to_fasta(step_3_histidine_df,step_3_fasta_path)
+df_to_fasta(step_3_histidine_df,step_3_fasta_path)
+
 
 
 
