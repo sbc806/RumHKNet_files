af34fc6f3d5e45fe9dbb25fa630266c704a367ff
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-16 16:17:58 -0800

    Update step_4_fasta.py

--- predictions_files/generate_fasta/step_4_fasta.py
+++ predictions_files/generate_fasta/step_4_fasta.py
@@ -38,6 +38,26 @@ predictions_information(newrun_complete_small_df)
 newrun_large_df=pd.read_csv(os.path.join(predictions_path,"newrun_seqs_large_histidine_kinase_batch_predicted_03.csv"))
 print(newrun_large_df.columns)
 predictions_information(newrun_large_df)
+newrun_complete_small_df_selected=newrun_complete_small_df.iloc[:,0:2]
+newrun_complete_small_df_selected["pred"]=newrun_complete_small_df.iloc[:,3]
+newrun_large_df_selected=newrun_large_df.iloc[:,0:2]
+newrun_large_df_selected["pred"]=newrun_large_df.iloc[:,3]
+newrun_complete_small_df_new=add_label(newrun_complete_small_df_selected)
+newrun_large_df_new=add_label(newrun_large_df_selected)
+def df_to_fasta(df,fasta_path):
+  with open(fasta_path,"a") as f:
+    for i in range(0,len(df)):
+      seq=df.iloc[i]
+      seq_id["seq_id"].iloc[i]
+      pred=df.iloc[:,2].iloc[i]
+      pred_other=df.iloc[:,3].iloc[i]
+      f.write(">{seq_id},{seq},{pred},{pred_other}\n")
+      f.write(seq)
+      if i<len(df)-1:
+        f.write("\n")
 
+step_4_df=pd.concat([clustered_all_df,newrun_all_df])
+step_4_fasta_path="../../../RumHKNet_fasta/step_4_histidine_kinase_batch_clustered_newrun.fasta")
+df_to_fasta(step_4_df,step_4_fasta_path)
 
 
