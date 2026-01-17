b1e113ce51cf727d67fdd0442348b69fefc8f71b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-16 16:02:16 -0800

    Update step_4_fasta.py

--- predictions_files/generate_fasta/step_4_fasta.py
+++ predictions_files/generate_fasta/step_4_fasta.py
@@ -19,3 +19,22 @@ for f in os.listdir(dir_1_path):
   df=pd.read_csv(os.path.join(dir_1_path,f))
   small_dfs.append(df)
   
+clustered_complete_small_df=pd.concat(small_dfs)
+print(clustered_complete_small_df)
+predictions_information(clustered_complete_small_df)
+clustered_large_df=pd.read_csv(os.path.join(predictions_path,"clustered_rep95_seq_large_histidine_kinase_batch_predicted_03.csv"))
+print(clustered_large_df.columns)
+predictions_information(clustered_large_df)
+
+dir_path=os.path.join(predictions_path,"newrun_seqs_small_histidine_kinase_batch")
+small_dfs=[]
+for f in os.listdir(dir_path):
+  df=pd.read_csv(os.path.join(dir_path,f))
+  small_dfs.append(df)
+newrun_complete_small_df=pd.concat(small_dfs)
+print(newrun_complete_small_df.columns)
+predictions_information(newrun_complete_small_df)
+
+newrun_large_df=pd.read_csv(os.path.join(predictions_path,"newrun_seqs_large_histidine_kinase_batch_predicted_03.csv"))
+print(newrun_large_df.columns)
+predictions_information(newrun_large_df)
