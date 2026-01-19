d7d103ece2d528776b47774ae53e84b8dc2c76ed
sbc806 <sarahc19233@gmail.com>
2026-01-19 18:37:38 -0500

    Made new directory.

---
+++ predictions_files/ko_blastp3050/check_other_step_2.py
@@ -0,0 +1,172 @@
+import os as os
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append("..")
+from predictions_helpers import predictions_information
+
+
+dataset_path="../../predictions/predictions_dataset/step_2/clustered"
+predictions_path="../../predictions/predicted_results/step_2/both/clustered"
+
+def get_predictions_df(predictions_path,i):
+  prediction_files=os.listdir(predictions_path)
+  selected_files=[f for f in prediction_files if "small_kinase_"+str(i)+"_"in f]
+  selected_files=sorted(selected_files,key=lambda x:int(x.split(".csv")[0].split("_")[-1]))
+  print("Length:",len(selected_files))
+  df=None
+  for f in selected_files:
+    current_df=pd.read_csv(os.path.join(predictions_path,f))
+    if df is None:
+      df=current_df
+    else:
+      df=pd.concat([df,current_df])
+  return df
+
+
+total_ko_clustered=pd.read_csv("../../histidine_other_software/total_KO_clustered_rep_seq95_shared.txt")
+total_ko_newrun=pd.read_csv("../../histidine_other_software/total_KO_newrun_seqs_shared.txt")
+total_blastp3050_clustered=pd.read_csv("../../histidine_other_software/total_blastp3050_clustered_rep_seq95_shared.txt")
+total_blastp3050_newrun=pd.read_csv("../../histidine_other_software/total_blastp3050_newrun_seqs_shared.txt")
+print(len(total_ko_clustered),len(total_ko_newrun))
+print(len(total_blastp3050_clustered),len(total_blastp3050_newrun))
+
+print("clustered_rep_seq95")
+print()
+small_dfs=[]
+i_df={}
+seq_ids=np.array([])
+seqs=np.array([])
+for i in range(0,4):
+  dir_i="small_kinase_"+str(i)
+  predictions_path_i=os.path.join(predictions_path,dir_i)
+  df_i=get_predictions_df(predictions_path_i,i)
+  
+  print(i)
+  if df_i is not None:
+    file_name=f"clustered_rep_seq95_small_kinase_{i}.csv"
+    # dataset_i=pd.read_csv(os.path.join(dataset_path,file_name)).iloc[:len(df_i)]
+    small_dfs.append(df_i)
+    
+    i_df[i]=df_i
+    seq_ids=np.hstack((seq_ids,df_i["seq_id"]))
+    seqs=np.hstack((seqs,df_i["seq"]))
+  else:
+    print("None")
+  print()
+
+complete_small_df=pd.concat(small_dfs)
+predictions_information(complete_small_df)
+num_small_histidine_kinases=np.sum(complete_small_df.iloc[:,3]==1)
+print("Number of histidine kinases <= 1500:",num_small_histidine_kinases)
+print()
+
+def df_to_fasta(df,fasta_path):
+  with open(fasta_path,"a") as f:
+    for i in range(0,len(df)):
+      seq_id=df["seq_id"].iloc[i]
+      seq=df["seq"].iloc[i]
+      f.write(">"+seq_id+"\n")
+      f.write(seq)
+      if i < len(df)-1:
+        f.write("\n")
+
+
+large_df_predicted=pd.read_csv(os.path.join(predictions_path,"clustered_rep_seq95_large_kinase_predicted_03.csv"),header=None)
+large_df_predicted.columns=["seq_id","seq","prob","label"]
+predictions_information(large_df_predicted)
+num_large_histidine_kinases=np.sum(large_df_predicted.iloc[:,3]==1)
+print("Number of histidine kinases > 1500:",num_large_histidine_kinases)
+print()
+
+large_df_predicted.columns=complete_small_df.columns
+clustered_rep_seq95_all_df=pd.concat([complete_small_df,large_df_predicted])
+predictions_information(clustered_rep_seq95_all_df)
+print()
+clustered_rep_seq95_all_df=clustered_rep_seq95_all_df[clustered_rep_seq95_all_df.iloc[:,3]==0]
+print("total_KO")
+total_ko_clustered_selected_df=clustered_rep_seq95_all_df[clustered_rep_seq95_all_df["seq_id"].isin(total_ko_clustered["seq_id"].values)]
+print(len(clustered_rep_seq95_all_df),len(total_ko_clustered),len(total_ko_clustered_selected_df))
+bins,counts=np.histogram(total_ko_clustered_selected_df["prob"],bins=3)
+print(bins,counts)
+print("total_blast3050")
+total_blastp3050_clustered_selected_df=clustered_rep_seq95_all_df[clustered_rep_seq95_all_df["seq_id"].isin(total_blastp3050_clustered["seq_id"].values)]
+print(len(clustered_rep_seq95_all_df),len(total_blastp3050_clustered),len(total_blastp3050_clustered_selected_df))
+bins,counts=np.histogram(total_blastp3050_clustered_selected_df["prob"],bins=3)
+print(bins,counts)
+
+print("newrun_seqs")
+print()
+desired=[i for i in range(0,2)]+['remaining']
+small_dfs=[]
+i_df={}
+for i in desired:
+  dir=f"newrun_seqs_small_kinase_{i}"
+  print(i)
+  df=get_predictions_df(os.path.join(predictions_path,dir),i)
+  small_dfs.append(df)
+  i_df[i]=df
+complete_small_df=pd.concat(small_dfs)
+# predictions_information(complete_small_df)
+
+
+histidine=complete_small_df.iloc[:,3]==1
+print("Number of histidine kinases <= 1500:",np.sum(histidine))
+
+print()
+
+large_df=pd.read_csv(os.path.join(predictions_path,"newrun_seqs_large_kinase_predicted_03.csv"))
+# predictions_information(large_df)
+histidine=large_df.iloc[:,3]==1
+print("Number of histidine kinases > 1500:",np.sum(histidine))
+
+print()
+
+large_df.columns=complete_small_df.columns
+newrun_seqs_all_df=pd.concat([complete_small_df,large_df])
+predictions_information(newrun_seqs_all_df)
+
+print()
+newrun_seqs_all_df=newrun_seqs_all_df[newrun_seqs_all_df.iloc[:,3]==0]
+print("total_KO")
+total_ko_newrun_selected_df=newrun_seqs_all_df[newrun_seqs_all_df["seq_id"].isin(total_ko_newrun["seq_id"].values)]
+print(len(newrun_seqs_all_df),len(total_ko_newrun),len(total_ko_newrun_selected_df))
+bins,counts=np.histogram(total_ko_newrun_selected_df["prob"],bins=3)
+print(bins,counts)
+print("total_blastp3050")
+total_blastp3050_newrun_selected_df=newrun_seqs_all_df[newrun_seqs_all_df["seq_id"].isin(total_blastp3050_newrun["seq_id"].values)]
+print(len(newrun_seqs_all_df),len(total_blastp3050_newrun),len(total_blastp3050_newrun_selected_df))
+bins,counts=np.histogram(total_blastp3050_newrun_selected_df["prob"],bins=3)
+print(bins,counts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
