b133f6df5321a71a463b79a71a83fea4c9ecf242
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-19 09:52:52 -0800

    Update check_other_step_1.py

--- predictions_files/check_other_step_1.py
+++ predictions_files/check_other_step_1.py
@@ -17,9 +17,10 @@ total_blastp3050_newrun_seqs=pd.read_csv("../../histidine_other_software/total_b
 print(len(total_blastp3050_clustered_rep_seq95)),len(total_blastp3050_newrun_seqs))
 predictions_information(total_blastp3050_clustered_rep_seq95)
 predictions_information(total_blastp3050_newrun_seqs)
-bins,counts=np.histogram(total_blastp3050_clustered_rep_seq95)
+bins,counts=np.histogram(total_blastp3050_clustered_rep_seq95,bins=10)
 print(bins,counts)
-bins,counts=np.histogram(total_blastp3050_newrun_seqs)
+bins,counts=np.histogram(total_blastp3050_newrun_seqs,bins=10)
 print(bins,counts)
+
 
 
