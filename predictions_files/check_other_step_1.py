134e134cef6d58c3f04a3769231882116be1f0aa
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-19 09:56:49 -0800

    Update check_other_step_1.py

--- predictions_files/check_other_step_1.py
+++ predictions_files/check_other_step_1.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 total_ko_clustered_rep_seq95=pd.read_csv("../../histidine_other_software/total_KO_clustered_rep_seq95_shared.txt")
 total_ko_newrun_seqs=pd.read_csv("../../histidine_other_software/total_KO_newrun_seqs_shared.txt")
-print(len(total_ko_clustered),len(total_ko_newrun_seqs))
+print(len(total_ko_clustered_rep_seq95),len(total_ko_newrun_seqs))
 predictions_information(total_ko_clustered_rep_seq95)
 predictions_information(total_ko_newrun_seqs)
-bins,counts=np.histogram(total_ko_clustered_rep_seq95,bins=10)
+bins,counts=np.histogram(total_ko_clustered_rep_seq95["prob"],bins=10)
 print(bins,counts)
-bins,counts=np.histogram(total_ko_newrun_seqs,bins=10)
+bins,counts=np.histogram(total_ko_newrun_seqs["prob"],bins=10)
 print(bins,counts)
 print()
 total_blastp3050_clustered_rep_seq95=pd.read_csv("../../histidine_other_software/total_blastp3050_clustered_rep_seq95_shared.txt")
@@ -17,10 +17,11 @@ total_blastp3050_newrun_seqs=pd.read_csv("../../histidine_other_software/total_b
 print(len(total_blastp3050_clustered_rep_seq95),len(total_blastp3050_newrun_seqs))
 predictions_information(total_blastp3050_clustered_rep_seq95)
 predictions_information(total_blastp3050_newrun_seqs)
-bins,counts=np.histogram(total_blastp3050_clustered_rep_seq95,bins=10)
+bins,counts=np.histogram(total_blastp3050_clustered_rep_seq95["prob"],bins=10)
 print(bins,counts)
-bins,counts=np.histogram(total_blastp3050_newrun_seqs,bins=10)
+bins,counts=np.histogram(total_blastp3050_newrun_seqs["prob"],bins=10)
 print(bins,counts)
+
 
 
 
