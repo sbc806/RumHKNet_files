170edeb5f9054ebf54577ccfe1de59a459652b16
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-19 18:27:01 -0800

    Update compare_common_seq.py

--- predictions_files/compare_common_seq.py
+++ predictions_files/compare_common_seq.py
@@ -19,13 +19,13 @@ newrun_df=pd.read_csv(os.path.join(predictions_dataset_path,"newrun_seqs.csv"))
 print("Number of sequences in common between clustered_rep_seq95 and newrun_seqs:",np.sum(clustered_df["seq_id"].isin(newrun_df["seq_id"].values)))
 print("Number of sequences in common between clustered_rep_seq95 and new:", np.sum(clustered_df["seq_id"].isin(new_df["seq_id"].values)))
 print("Number of sequences in common between newrun_seqs and new:",np.sum(newrun_df["seq_id"].isin(new_df["seq_id"].values)))
-
+"""
 contained_1=new_df["seq_id"].isin(clustered_rep_seq95_df["seq_id"].values)
 contained_2=new_df["seq_id"].isin(newrun_df["seq_id"].values)
 extra=~contained_1&~contained_2
 print(np.sum(contained_1),np.sum(contained_2),np.sum(extra))
 new_df[extra].to_csv("../../new/clustered95_RBAGs_extra.csv",index=False)
-
+"""
 total_ko=pd.read_csv("../../histidine_other_software/total_KO.txt",header=None)
 total_blastp3050=pd.read_csv("../../histidine_other_software/total_blastp3050.txt",header=None)
 print(len(total_ko),np.unique(total_ko[0]).shape)
@@ -62,6 +62,7 @@ get_information(new_df,total_blastp3050,clustered_df,newrun_df,"total_blastp3050
 # Seuqneces not in clustered_rep_seq95, newrun_seqs, exclusive totalKO, and total blastp3050
 new_only=(~new_df["seq_id"].isin(total_KO[0]))&(~new_df["seq_id"].isin(total_blastp3050[0]))&(~new_df["seq_id"].isin(clustered_df["seq_id"].values))&(~new_df["seq_id"].isin(newrun_df["seq_id"].values))
 new_df[new_only].to_csv("../../new/clustered_RBAGs_only.csv",index=False)
+
 
 
 
