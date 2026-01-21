00d6aef0d9b5f101173d4c50026006cdc0b6e5e8
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-20 20:11:36 -0800

    Update check_common.py

--- predictions_files/generate_fasta/check_common.py
+++ predictions_files/generate_fasta/check_common.py
@@ -4,10 +4,13 @@ import numpy as np
 
 step_1_predicted_df=pd.read_csv("../../../RumHKNet_csv/step_1_clustered_newrun_rbags_predicted_03.csv")
 clustered95_rbags=pd.read_csv("../../../new/clustered95_RBAGs.csv")
-
-seq_id_argsort=np.argsort(step_1_predicted_df["seq_id"].values)
-step_1_predicted_df=step_1_predicted_df.iloc[seq_id_argsort]
-clustered95_rbags=clustered95_rbags.iloc[seq_id_argsort]
+print(step_1_predicted_df)
+print(clustered95_rbags)
+seq_id_argsort_1=np.argsort(step_1_predicted_df["seq_id"].values)
+step_1_predicted_df=step_1_predicted_df.iloc[seq_id_argsort_1]
+seq_id_argsort_2=np.argsort(clustered95_rbags["seq_id"].values)
+clustered95_rbags=clustered95_rbags.iloc[seq_id_argsort_2]
 print(len(step_1_predicted_df),len(clustered95_rbags))
 print(np.sum(step_1_predicted_df["seq_id"].values==clustered95_rbags["seq_id"].values))
 print(np.sum(step_1_predicted_df["seq"].values==clustered95_rbags["seq"].values))
+
