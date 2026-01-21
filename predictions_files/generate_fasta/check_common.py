c5165a2b3261bd2eb9aa962519f3d9fee5629e01
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-20 20:21:42 -0800

    Update check_common.py

--- predictions_files/generate_fasta/check_common.py
+++ predictions_files/generate_fasta/check_common.py
@@ -2,7 +2,9 @@ import os as os
 import pandas as pd
 import numpy as np
 
-step_1_predicted_df=pd.read_csv("../../../RumHKNet_csv/step_1_clustered_newrun_rbags_predicted_03.csv")
+step_1_predicted_df=pd.read_csv("../../../RumHKNet_csv/step_1_clustered_newrun_rbags_predicted_03_old.csv")
+step_1_predicted_df.iloc[:,1:].to_csv("../../../RumHKNet_csv/step_1_clustered_newrun_rbags_predicted_03.csv")
+"""
 clustered95_rbags=pd.read_csv("../../../new/clustered95_RBAGs.csv")
 print(step_1_predicted_df)
 print(clustered95_rbags)
@@ -13,4 +15,5 @@ clustered95_rbags=clustered95_rbags.iloc[seq_id_argsort_2]
 print(len(step_1_predicted_df),len(clustered95_rbags))
 print(np.sum(step_1_predicted_df["seq_id"].values==clustered95_rbags["seq_id"].values))
 print(np.sum(step_1_predicted_df["seq"].values==clustered95_rbags["seq"].values))
+"""
 
