50c196ac2962455d6f11762eb5ec345577aeade5
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-20 19:52:32 -0800

    Update check_common.py

---
+++ predictions_files/generate_fasta/check_common.py
@@ -0,0 +1,6 @@
+import os as os
+impotr pt pandas as pd
+import numpy as np
+
+step_1_predicted_df=pd.read_csv("../../../RumHKNet_csv/step_1_clustered_newrun_rbags_predicted_03.csv")
+clustered95_rbags=pd.read_csv("../../../new/clustered95_RBAGs.csv")
