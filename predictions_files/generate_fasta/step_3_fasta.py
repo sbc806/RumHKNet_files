35b5644b8b22e3f2ba1503b655956d4589dfa181
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-23 13:02:07 -0800

    Update step_3_fasta.py

--- predictions_files/generate_fasta/step_3_fasta.py
+++ predictions_files/generate_fasta/step_3_fasta.py
@@ -155,21 +155,24 @@ def df_to_fasta(df,fasta_path):
         f.write("\n")
 # step_3_histidine_df=pd.concat([clustered_histidine_all_df,newrun_histidine_all_df])
 
-step_2_predicted_rbag_df=pd.read_csv("../../../RumHKNet_csv/step_2_clustered_newrun_rbag_predicted_03.csv")
-contained_1=clustered_histidine_all_df["seq_id"].isin(step_2_predicted_rbag_df["seq_id"].values))
+step_2_predicted_rbag_df=pd.read_csv("../../../RumHKNet_csv/step_2_clustered_newrun_rbags_predicted_03.csv")
+step_2_predicted_rbag_histidine_df=step_2_predicted_rbag_df[step_2_predicted_rbag_df.iloc[:,3]==1]
+
+contained_1=clustered_histidine_all_df["seq_id"].isin(step_2_predicted_rbag_histidine_df["seq_id"].values))
 clustered_histidine_new_df=clustered_histidine_all_df[contained_1]
-contained_2=newrun_histidine_all_df["seq_id"].isin(step_2_predicted_rbag_df["seq_id"].values))
+contained_2=newrun_histidine_all_df["seq_id"].isin(step_2_predicted_rbag_histidine_df["seq_id"].values))
 newrun_histidine_new_df=newrun_histidine_all_df[contained_2]
-print(np.sum(step_2_predicted_rbag_df.iloc[:,3]==1),len(clustered_histidine_new_df),len(newrun_histidine_new_df),len(clustered_histidine_new_df)+len(newrun_histidine_new_df))
+print(np.sum(step_2_predicted_rbag_histidine_df),len(clustered_histidine_new_df),len(newrun_histidine_new_df),len(clustered_histidine_new_df)+len(newrun_histidine_new_df))
 
 step_3_histidine_df=pd.concat([clustered_histidine_new_df,newrun_histidine_new_df])
 predictions_information(step_3_histidine_df)
 print(np.unique(step_3_histidine_df.iloc[:,2]))
 print(np.unique(step_3_histidine_df.iloc[:,3]))
-step_3_histidine_df.to_csv("../../../RumHKNet_csv/step_3_clustered_newrun_rbag_predicted_03.csv",index=False)
+step_3_histidine_df.to_csv("../../../RumHKNet_csv/step_3_clustered_newrun_rbags_predicted_03.csv",index=False)
 # step_3_fasta_path="../../../RumHKNet_fasta/step_3_kinase_family_clustered_newrun.fasta"
-step_3_fasta_path="../../../RumHKNet_fasta/step_3_kinase_family_clustered_newrun_rbag.fasta"
+step_3_fasta_path="../../../RumHKNet_fasta/step_3_kinase_family_clustered_newrun_rbags.fasta"
 df_to_fasta(step_3_histidine_df,step_3_fasta_path)
+
 
 
 
