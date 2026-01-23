705d06960392d14d57faad035cae1fdb8b702399
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-23 14:41:25 -0800

    Update check_overlapping_step_2.py

--- predictions_files/ko_blastp3050/check_overlapping_step_2.py
+++ predictions_files/ko_blastp3050/check_overlapping_step_2.py
@@ -25,7 +25,7 @@ print("total_blastp3050")
 total_blastp3050_small_dir_path_0=os.path.join(predictions_path,'total_blastp3050_small_kinase_0')
 total_blastp3050_small_dir_path_1=os.path.join(predictions_path,'total_blastp3050_small_kinase_1')
 total_blastp3050_small_dfs=[]
-for f in os.listdir(total_blastp3050_small_dir_path_0)[:-1]:
+for f in sorted(os.listdir(total_blastp3050_small_dir_path_0)[:-1]):
   df=pd.read_csv(os.path.join(total_blastp3050_small_dir_path_0,f))
   # print(f,df)
   total_blastp3050_small_dfs.append(df)
@@ -48,6 +48,7 @@ total_blastp3050_df=pd.concat([total_blastp3050_small_df,total_blastp3050_large_
 predictions_information(total_blastp3050_df)
 
 predictions_information(step_2_predicted_rbag_df)
+
 
 
 
