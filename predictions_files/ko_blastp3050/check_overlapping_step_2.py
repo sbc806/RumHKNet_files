11538c996d1dac0806e53b8d31fe5a6b719a061b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-23 14:26:01 -0800

    Update check_overlapping_step_2.py

--- predictions_files/ko_blastp3050/check_overlapping_step_2.py
+++ predictions_files/ko_blastp3050/check_overlapping_step_2.py
@@ -17,16 +17,16 @@ for f in os.listdir(total_ko_small_dir_path):
   df=pd.read_csv(os.path.join(total_ko_small_dir_path,f))
   total_ko_small_dfs.apepnd(df)
 
-total_ko_large_path=os.path.join(predictions_path,"2025_01_20_new_ko_shared_laarge_predicted_03.csv")
+total_ko_large_path=os.path.join(predictions_path,"2025_01_20_new_ko_shared_large_predicted_03.csv")
 total_ko_large_df=pd.read_csv(total_ko_large_path)
 
 total_blastp3050_small_dir_path_0=os.path.join(predictions_path,'total_blastp3050_small_kinase_0')
 total_blastp3050_small_dir_path_1=os.path.join(redictions_path,'total_blastp3050_small_kinase_1')
-total_blastp3050_dfs=[]
+total_blastp3050_small_dfs=[]
 for f in os.listdir(total_blastp3050_small_dir_path_0):
   df=pd.read_csv(os.path.join(total_blastp3050_small_dir_path_0,f))
   total_blastp3050_small_dfs.append(df)
-for f in oslistdir(total_blastp3050_small_dir_path_1):
+for f in os.listdir(total_blastp3050_small_dir_path_1):
   df=pd.read_csv(os.path.join(total_blastp3050_small_dir_path_1,f))
   total_blastp3050_small_dfs.append(df)
 
@@ -43,3 +43,4 @@ total_blastp3050_large_df.columns=total_blastp3050_small_df.columns
 total_blastp3050_df=pd.concat([total_blastp3050_small_df,total_blastp3050_large_df])
 predictions_information(total_blastp3050_df)
 
+predictions_information(step_2_rbag_predicted_df)
