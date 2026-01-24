1ce5614c43aa0e740cec8ba159501e715fb6a253
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-23 21:48:45 -0800

    Update generate_step_2_data_splits_02.py

--- data_splits/generate_step_2_data_splits_02.py
+++ data_splits/generate_step_2_data_splits_02.py
@@ -6,12 +6,13 @@ step_1_rbag_02_df=pd.read_csv("../../predictions/predictions_dataset/step_2/clus
 split_size=900000
 num_splits=len(step_1_rbag_02_df)//split_size
 dfs=[]
-for i in range(0,len(num_splits)):
+for i in range(0,num_splits):
   start=i*split_size
   end=start+split_size
   step_1_rbag_02_df_i=step_1_rbag_02_df[start:end]
   step_1_rbag_02_df_i.to_csv(f"../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained_{i}.csv",index=False)
   dfs.append(step_1_rbag_02_df_i)
 print(len(step_1_rbag_02_df),len(pd.concat(dfs)))
+
 
 
