37bd2b689181d602ef129090bf5c8188787e4474
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-23 21:47:01 -0800

    Update generate_step_2_data_splits_02.py

--- data_splits/generate_step_2_data_splits_02.py
+++ data_splits/generate_step_2_data_splits_02.py
@@ -8,5 +8,7 @@ num_splits=len(step_1_rbag_02_df)//split_size
 for i in range(0,len(num_splits)):
   start=i*split_size
   end=start+split_size
-  step_1_rbag_02_df.to_csv(f"../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained_{i}.csv",index=False)
+  step_1_rbag_02_df_i=step_1_rbag_02_df[start:end]
+  step_1_rbag_02_df_i.to_csv(f"../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained_{i}.csv",index=False)
+
 
