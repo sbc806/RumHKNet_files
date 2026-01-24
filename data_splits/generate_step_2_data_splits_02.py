86727d448b32657d143230762a6e2aee89eddf01
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-23 21:47:53 -0800

    Update generate_step_2_data_splits_02.py

--- data_splits/generate_step_2_data_splits_02.py
+++ data_splits/generate_step_2_data_splits_02.py
@@ -5,10 +5,13 @@ import numpy as np
 step_1_rbag_02_df=pd.read_csv("../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained.csv")
 split_size=900000
 num_splits=len(step_1_rbag_02_df)//split_size
+dfs=[]
 for i in range(0,len(num_splits)):
   start=i*split_size
   end=start+split_size
   step_1_rbag_02_df_i=step_1_rbag_02_df[start:end]
   step_1_rbag_02_df_i.to_csv(f"../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained_{i}.csv",index=False)
+  dfs.append(step_1_rbag_02_df_i)
+print(len(step_1_rbag_02_df),len(pd.concat(dfs)))
 
 
