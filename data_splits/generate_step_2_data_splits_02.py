aa3ba4ca095408a5dfec599c4357839102696d8a
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-24 21:50:11 -0800

    Update generate_step_2_data_splits_02.py

--- data_splits/generate_step_2_data_splits_02.py
+++ data_splits/generate_step_2_data_splits_02.py
@@ -6,17 +6,21 @@ sys.path.append("..")
 from predictions_files.predictions_helpers import predictions_information
 
 step_1_rbag_02_df=pd.read_csv("../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained.csv")
-predictions_nformation(step_1_rbag_02_df)
+predictions_information(step_1_rbag_02_df)
+step_1_rbag_02_small_df=step_1_rbag_02_df[step_1_rbag_02_df["seq"].str.len()<=1500]
+step_1_rbag_02_large_df=step_1_rbag_02_df[step_1_rbag_02_df["seq"].str.len()>1500]
+
 split_size=900000
-num_splits=len(step_1_rbag_02_df)//split_size+1
+num_splits=len(step_1_rbag_02_small_df)//split_size+1
 dfs=[]
 for i in range(0,num_splits):
   start=i*split_size
   end=start+split_size
-  step_1_rbag_02_df_i=step_1_rbag_02_df[start:end]
-  step_1_rbag_02_df_i.to_csv(f"../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained_{i}.csv",index=False)
-  dfs.append(step_1_rbag_02_df_i)
-print(len(step_1_rbag_02_df),len(pd.concat(dfs)))
+  step_1_rbag_02_small_df_i=step_1_rbag_02_small_df[start:end]
+  step_1_rbag_02_small_df_i.to_csv(f"../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained_{i}.csv",index=False)
+  dfs.append(step_1_rbag_02_small_df_i)
+print(len(step_1_rbag_02_small_df),len(pd.concat(dfs)))
+
 
 
 
