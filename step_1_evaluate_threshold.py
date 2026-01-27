30c9826dc1390e74fcd23948cc211bf84fba5a43
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-26 16:31:32 -0800

    Update step_1_evaluate_threshold.py

--- step_1_evaluate_threshold.py
+++ step_1_evaluate_threshold.py
@@ -45,8 +45,8 @@ for threshold in thresholds:
   print("Threshold:",threshold,"Accuracy:",accuracy)
 
 print("Validation accuracy")
-step_1_dev_df=pd.read_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_class/dev/dev.csv")
-step_1_non_kinases_preprocessed_dev_predicted_df=pd.read_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/step_1_non_kinases_preprocessed_dev_predicted_03_v2.csv")
+step_1_dev_df=pd.read_csv(os.path.join(dir_path,"kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_class/dev/dev.csv"))
+step_1_non_kinases_preprocessed_dev_predicted_df=pd.read_csv(os.path.join(dir_path,"predictions/predicted_results/step_1/both/step_1_non_kinases_preprocessed_dev_predicted_03_v2.csv"))
 for threshold in thresholds:
   predictions=step_1_non_kinases_preprocessed_dev_predicted_df["prob"]>=threshold
   labels=step_1_dev_df["label"]
