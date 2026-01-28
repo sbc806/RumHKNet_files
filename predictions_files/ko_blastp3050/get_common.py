9a65b0c6e7401a7732709d9f57de87651a6c3d2d
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-27 21:27:54 -0800

    Update get_common.py

---
+++ predictions_files/ko_blastp3050/get_common.py
@@ -0,0 +1,14 @@
+import os as os
+import pandas as pd
+import numpy as np
+
+step_1_clustered-newrun_rbags_df=pd.read_csv("../../../RumHKNet_csv/step_1_clustered_newrun_rbags_predicted_03.csv")
+
+total_ko_df=pd.read_csv("../../../histidine_other_software/total_KO.txt",header=None)
+total_blastp3050_df=pd.read_csv("../../../histidine_other_software/total_vlastp3050.txt",header=None)
+
+total_ko_contained=total_ko_df.isin(stpe_1_clustered_newrun_rbags_df["seq_id"].values)
+total_blastp3050_contained=total_blastp3050_df.isin(step_1_clustered_newrun_rbags_df["seq_id"].values)
+
+print(len(total_ko_df),np.unique(total_ko_df[0]).shape,np.sum(total_ko_contained))
+print(len(total_blastp3050_df),np.unique(total_blastp3050_df[0]).shape,np.sum(total_blastp3050_contained))
