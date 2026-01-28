20e32a109bf098afae925bd1136e90b1451035fb
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-27 19:50:20 -0800

    Update step_2_1_fasta.py

--- predictions_files/generate_fasta/step_2_1_fasta.py
+++ predictions_files/generate_fasta/step_2_1_fasta.py
@@ -18,10 +18,12 @@ for i in range(0,3):
   small_dir_path=os.path.join(predictions_path,small_dir)
   small_df=create_df(small_dir_path)
   small_dfs.append(small_df)
+complete_small_df=pd.concat(small_dfs)
 
 large_dir_path=os.path.join(predictions_path,"step_1_clustered_newrun_rbags_02_not_contained_large_predicted_03_v2.csv")
 large_df=pd.read_csv(large_dir_path)
 
-step_1_not_contained_02_df=pd.concat([complete_small_df,large_df])
+step_1_02_not_contained_df=pd.concat([complete_small_df,large_df])
 step_1_df=pd.read_csv("../../../")
+
 
