e8a6eed01be874064a31a2892644a4c823bc9a33
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-27 20:18:41 -0800

    Update step_2_1_fasta.py

--- predictions_files/generate_fasta/step_2_1_fasta.py
+++ predictions_files/generate_fasta/step_2_1_fasta.py
@@ -44,7 +44,7 @@ predictions_information(step_2_predicted_df)
 
 step_2_predicted_df.to_csv("../../../RumHKNet_csv/step_2_clustered_newrun_rbags_predicted_02.csv",index=False)
 
-def df_to_fasta(df):
+def df_to_fasta(df,save_path):
   with open(save_path,"a") as f:  
     for i in range(0,len(df)):
       seq_id=df["seq_id"].iloc[i]
@@ -53,9 +53,12 @@ def df_to_fasta(df):
       f.write(seq+"\n")
       if i<len(df)-1:
         f.write("\n")
-        
+
+df_to_fasta(step_2_predicted_df,"../../../RumHKNet_fasta/step_2_clustered_newrun_rbags")
+
 print("Number of histidine kinases for a threshold of 0.3:",np.sum(step_2_predicted_df["prob"]>=0.3))
 print("Number of histidine kinases for a threshold of 0.2:",np.sum(step_2_predicted_df["prob"]>=0.2))
+
 
 
 
