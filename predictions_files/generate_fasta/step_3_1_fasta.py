5af19a5bedc1c95db3f3667018198511f955062e
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-30 15:49:10 -0800

    Update step_3_1_fasta.py

--- predictions_files/generate_fasta/step_3_1_fasta.py
+++ predictions_files/generate_fasta/step_3_1_fasta.py
@@ -126,9 +126,9 @@ print(step_3_02_df_new)
 print(np.unique(step_3_02_df_new["pred"]))
 print(np.unique(step_3_02_df_new["pred_other"]))
 
-# step_3_02_df.to_csv("../../../RumHKNet_csv/step_3_clustered_newrun_rbags_predicted_02.csv")
+step_3_02_df_new.to_csv("../../../RumHKNet_csv/step_3_clustered_newrun_rbags_predicted_02.csv")
 
-# df_to_fasta(step_3_02_df_new,"../../../RumHKNet_fasta/step_3_histidine_kinase_family_clustered_newrun_rbags_674002.fasta")
+df_to_fasta(step_3_02_df_new,"../../../RumHKNet_fasta/step_3_histidine_kinase_family_clustered_newrun_rbags_674002.fasta")
 
 """
 remaining=step_3_02_df["seq_id"].isin(step_3_02_remaining_df["seq_id"].values)
@@ -153,6 +153,7 @@ print(step_3_02_large_remaining_df_new)
 step_3_02_small_remaining_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun_rbags_02_small_remaining_batch.csv",index=False)
 step_3_02_large_remaining_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun_rbags_02_large_remaining_batch.csv",index=False)
 """
+
 
 
 
