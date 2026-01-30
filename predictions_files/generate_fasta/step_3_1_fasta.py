14816f6c3a2f9413736d0fb4199af1e166df5e74
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-29 22:58:50 -0800

    Update step_3_1_fasta.py

--- predictions_files/generate_fasta/step_3_1_fasta.py
+++ predictions_files/generate_fasta/step_3_1_fasta.py
@@ -76,6 +76,7 @@ print(other_label)
 with open("../../../kinases_dataset/step_3_11_family/protein/multi_class/label.json","w") as f:
   json.dump(other_label,f)
 
+"""
 def df_to_fasta(df,fasta_path):
   with open(fasta_path,"a") as f:
     for i in range(0,len(df)):
@@ -83,8 +84,8 @@ def df_to_fasta(df,fasta_path):
       seq=df["seq"].iloc[i]
       f.write(f">{seq_id}"+"\n")
       f.write(f"{seq}"+"\n")
-
 """
+
 def df_to_fasta(df, fasta_path):
   with open(fasta_path,"a") as f:
     for i in range(0,len(df)):
@@ -94,7 +95,7 @@ def df_to_fasta(df, fasta_path):
       pred_other=df["pred_other"].iloc[i]
       f.write(f">{seq_id},{pred},{pred_other}"+"\n")
       f.write(f"{seq}\n")
-"""
+
 
 # step_1_02_df=pd.read_csv("../../../RumHKNet_csv/")
 step_3_03_df=pd.read_csv("../../../RumHKNet_csv/step_1_03_step_2_03/step_3_03/step_3_clustered_newrun_rbags_predicted_03.csv")
@@ -142,6 +143,7 @@ print(step_3_02_large_df_new)
 
 step_3_02_small_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun_rbags_02_small_remaining.csv",index=False)
 step_3_02_large_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun_rbags_02_large_remaining.csv",index=False)
+
 
 
 
