d65594fcb445d5ae77bb662ea0c13ee27b007e5f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-30 00:28:22 -0800

    Update step_4_evaluate_predictions.py

--- evaluate_threshold/step_4_evaluate_predictions.py
+++ evaluate_threshold/step_4_evaluate_predictions.py
@@ -6,8 +6,8 @@ dataset_path="../../predictions/predictions_dataset/step_3/"
 predictions_path="../../predictions/predicted_results/step_3/both"
 
 
-step_4_dev=pd.read_csv("../../kinases_dataset/step_3_11_family/protein/multi_class/dev/dev.csv")
-step_4_dev_0=pd.read_csv(os.path.join(predictions_path,"step_3_11_family_dev_predicted_03.csv"))
+step_4_dev=pd.read_csv("../../kinases_dataset/extra_p_133_class_v3_batch/protein/multi_class/dev/dev.csv")
+step_4_dev_0=pd.read_csv(os.path.join(predictions_path,"extra_p_133_class_v3_batch_dev_predicted_03.csv"))
 # step_4_dev_1=pd.read_csv(os.path.join(predictions_path,"step_3_11_family_dev_predicted_03_3432_v4_0.csv"))
 
 print(step_4_dev_0)
@@ -38,6 +38,7 @@ for df in dfs:
   total=len(df)
   accuracy=correct/total
   print("Accuracy:",accuracy)
+
 
 
 
