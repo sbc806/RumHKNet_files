645a6a97a8ea77a5946acc4ef448dd739d69e695
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-29 22:54:46 -0800

    Update step_3_1_fasta.py

--- predictions_files/generate_fasta/step_3_1_fasta.py
+++ predictions_files/generate_fasta/step_3_1_fasta.py
@@ -132,14 +132,17 @@ step_3_02_large_df_new["batch"]=step_3_02_large_df_new["pred_other"].values
 
 other_families_small=step_3_02_small_df_new["pred_other"]==10
 other_families_large=step_3_02_large_df_new["pred_other"]==10
+print(np.sum(other_families_small))
+print(np.sum(other_families_large))
 step_3_02_small_df_new["batch"][other_families_small]=-1
 step_3_02_large_df_new["batch"][other_families_large]=-1
 
 print(step_3_02_small_df_new)
 print(step_3_02_large_df_new)
 
-step_3_02_small_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun02_remaining_small.csv",index=False)
-step_3_02_large_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clremaining_large.csv",index=False)
+step_3_02_small_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun_rbags_02_small_remaining.csv",index=False)
+step_3_02_large_df_new[["seq_id","seq","batch"]].to_csv("../../../predictions/predictions_dataset/step_4/clustered/step_4_clustered_newrun_rbags_02_large_remaining.csv",index=False)
+
 
 
 
