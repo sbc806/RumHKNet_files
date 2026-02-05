c991171ba784ef3d45fd3e6e57b795ed77c23f10
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-02-04 17:19:35 -0800

    Update get_step_1_predictions_02.py

--- predictions_files/get_step_1_predictions_02.py
+++ predictions_files/get_step_1_predictions_02.py
@@ -1,15 +1,16 @@
 import os as os
-
+import numpy as np
 import pandas as pd
 
 step_1_predictions_03_df=pd.read_csv("../../RumHKNet_csv/step_1_03_step_2_03/step_1_03/step_1_clustered_newrun_rbags_predicted_03.csv")
 print(step_1_predictions_03_df)
 step_1_predictions_03_df=step_1_predictions_03_df.iloc[:,0:3]
-step_1_predictions_03_df["pred"]=step_1_predictions_03_df["prob"]>=0.2
+step_1_predictions_03_df["pred"]=(step_1_predictions_03_df["prob"]>=0.2).astype(int)
 print(step_1_predictions_03_df)
 print(np.sum(step_1_predictions_03_df["pred"]))
 
 step_1_predictions_03_df.to_csv("../../RumHKNet_csv/step_1_02_step_2_02/step_1_clustered_newrun_rbags_predicted_02.csv",index=False)
+
 
 
 
