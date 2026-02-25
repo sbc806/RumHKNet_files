6bc3d8a629a227c0135733e5ccf87340042fbf9d
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-02-24 17:48:40 -0800

    Update check_overlapping_step_1.py

--- predictions_files/ko_blastp3050/check_overlapping_step_1.py
+++ predictions_files/ko_blastp3050/check_overlapping_step_1.py
@@ -14,41 +14,14 @@ step_1_predicted_rbag_kinase_df=step_1_predicted_rbag_df[step_1_predicted_rbag_d
 # step_2_predicted_rbag_histidine_df=step_2_predicted_rbag_df[step_2_predicted_rbag_df.iloc[:,3]==1]
 
 print("total_KO")
-total_ko_small_dir_path=os.path.join(predictions_path,"total_ko_small_kinase")
-total_ko_small_dfs=[]
-for f in os.listdir(total_ko_small_dir_path):
-  df=pd.read_csv(os.path.join(total_ko_small_dir_path,f))
-  # print(df)
-  total_ko_small_dfs.append(df)
-
-total_ko_large_path=os.path.join(predictions_path,"2025_01_20_new_ko_shared_large_predicted_03.csv")
-total_ko_large_df=pd.read_csv(total_ko_large_path)
+total_ko_df=pd.read_csv("../../../total_KO.txt",header=None)
+print(len(total_ko_df))
+print()
 
 print("total_blastp3050")
-total_blastp3050_small_dir_path_0=os.path.join(predictions_path,'total_blastp3050_small_kinase_0')
-total_blastp3050_small_dir_path_1=os.path.join(predictions_path,'total_blastp3050_small_kinase_1')
-total_blastp3050_small_dfs=[]
-for f in sorted(os.listdir(total_blastp3050_small_dir_path_0)[:-1]):
-  df=pd.read_csv(os.path.join(total_blastp3050_small_dir_path_0,f))
-  # print(f,df)
-  total_blastp3050_small_dfs.append(df)
-for f in os.listdir(total_blastp3050_small_dir_path_1):
-  df=pd.read_csv(os.path.join(total_blastp3050_small_dir_path_1,f))
-  # print(f,df)
-  total_blastp3050_small_dfs.append(df)
-
-total_blastp3050_large_path=os.path.join(predictions_path,"2025_01_20_new_blastp3050_shared_large_predicted_03.csv")
-total_blastp3050_large_df=pd.read_csv(total_blastp3050_large_path)
-
-total_ko_small_df=pd.concat(total_ko_small_dfs)
-total_ko_large_df.columns=total_ko_small_df.columns
-total_ko_df=pd.concat([total_ko_small_df,total_ko_large_df])
-predictions_information(total_ko_df)
-
-total_blastp3050_small_df=pd.concat(total_blastp3050_small_dfs)
-total_blastp3050_large_df.columns=total_blastp3050_small_df.columns
-total_blastp3050_df=pd.concat([total_blastp3050_small_df,total_blastp3050_large_df])
-predictions_information(total_blastp3050_df)
+total_blastp3050_df=pd.read_csv("../../../total_blastp3050.txt",header=None)
+print(len(total_blastp3050_df))
+print()
 
 # predictions_information(step_1_predicted_rbag_df)
 
@@ -58,6 +31,7 @@ print(len(total_blastp3050_df),np.sum(total_blastp3050_df["seq_id"].isin(step_1_
 total_ko_df["prob_step_1"]=step_1_predicted_rbag_df.loc[total_ko_df["seq_id"].values]["prob"].values
 total_blastp3050_df["prob_step_1"]=step_1_predicted_rbag_df.loc[total_blastp3050_df["seq_id"].values]["prob"].values
 
+""""
 print()
 print("Number of kinases predicted in step 1 using RumHKNet and threshold 0.3:",np.sum(step_1_predicted_rbag_df.iloc[:,3]==1))
 print("Number of kinases predicted in step 1 using RumHKNet and threshold 0.2:",np.sum(step_1_predicted_rbag_df.iloc[:,2]>=0.2))
@@ -92,6 +66,7 @@ step_1_predicted_rbag_kinase_02_df=step_1_predicted_rbag_df[step_1_predicted_rba
 not_contained=~step_1_predicted_rbag_kinase_02_df["seq_id"].isin(step_1_predicted_rbag_kinase_df["seq_id"].values)
 print("Number of kinases not contained:",np.sum(not_contained))
 # step_1_predicted_rbag_kinase_02_df[not_contained].to_csv("../../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained.csv",index=False)
+"""
 
 
 
