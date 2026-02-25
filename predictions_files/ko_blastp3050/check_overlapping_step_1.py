1d73e466fae5b9293096934ebf41a04f5f7e03fa
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-02-24 19:10:54 -0800

    Update check_overlapping_step_1.py

--- predictions_files/ko_blastp3050/check_overlapping_step_1.py
+++ predictions_files/ko_blastp3050/check_overlapping_step_1.py
@@ -41,7 +41,7 @@ print()
 total_ko_02=total_ko_df["prob_step_1"]>=0.2
 total_blastp3050_02=total_blastp3050_df["prob_step_1"]>=0.2
 print(len(total_ko_df),np.sum(total_ko_02),np.sum(~total_ko_02))
-print(len(total_blastp3050),np.sum(total_blastp3050_02),np.sum(~total_blastp3050_02))
+print(len(total_blastp3050_df),np.sum(total_blastp3050_02),np.sum(~total_blastp3050_02))
 """"
 print()
 print("Number of kinases predicted in step 1 using RumHKNet and threshold 0.3:",np.sum(step_1_predicted_rbag_df.iloc[:,3]==1))
@@ -78,6 +78,7 @@ not_contained=~step_1_predicted_rbag_kinase_02_df["seq_id"].isin(step_1_predicte
 print("Number of kinases not contained:",np.sum(not_contained))
 # step_1_predicted_rbag_kinase_02_df[not_contained].to_csv("../../../predictions/predictions_dataset/step_2/clustered/step_1_clustered_newrun_rbags_02_not_contained.csv",index=False)
 """
+
 
 
 
