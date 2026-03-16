e86aa0b3434b2e771f8e4b9824f7946954bee98b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-16 07:45:47 -0700

    Update step_1_step_2_combined_datasets.py

--- predictions_files/step_1_step_2_combined/step_1_step_2_combined_datasets.py
+++ predictions_files/step_1_step_2_combined/step_1_step_2_combined_datasets.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 
-histidine_02=pd.read_csv(os.path.join(dir_path,"step_1_02_step_2_02/step_3_clustered_newrun_rbags_predicted_02.csv"))
+histidine_02=pd.read_csv(os.path.join(dir_path,"RumHKNet_csv/step_1_02_step_2_02/step_3_clustered_newrun_rbags_predicted_02.csv"))
 
 ko=pd.read_csv(os.path.join(dir_path,"histidine_other_software/total_KO_95%.txt"))
 blastp=pd.read_csv(os.path.join(dir_path,"histidine_other_software/final_Blastp_HK95%_3050100.txt"))
