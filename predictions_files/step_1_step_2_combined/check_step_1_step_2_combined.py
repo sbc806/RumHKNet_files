9ee69d5f764df1fb99c0e41de0b107214fdab97e
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-17 18:14:41 -0700

    Update check_step_1_step_2_combined.py

--- predictions_files/step_1_step_2_combined/check_step_1_step_2_combined.py
+++ predictions_files/step_1_step_2_combined/check_step_1_step_2_combined.py
@@ -3,11 +3,19 @@ import numpy as np
 import pandas as pd
 
 def check_specific(dir_path,f_name):
-  files-os.listdir(dir_path)
-  selected_files=[fnfor f in files if f_name in f]
+  files=os.listdir(dir_path)
+  selected_files=[f for f in files if f_name in f]
 
   dfs=[]
   for f in selected_files:
     df=pd.read_csv(os.path.join(dir_path,f))
     dfs.append(df)
   return pd.concat(dfs)
+
+dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicteed_results/step_1_step_2_combined/both/clustered"
+
+ko=check_specific(dir_path,"histidine_ko_no_blastp_no_rumhknet")
+blastp=check_specific(dir_path,"histidine_blastp_ko_no_rumhknet")
+
+print("KO:",len(ko))
+print("Blastp:",len(blastp))
