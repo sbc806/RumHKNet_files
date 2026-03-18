36c59ff93a844eb5b5fe69a624ecc7a056cfe0f8
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-17 18:21:26 -0700

    Update step_1_step_2_combined_datasets.py

--- predictions_files/step_1_step_2_combined/step_1_step_2_combined_datasets.py
+++ predictions_files/step_1_step_2_combined/step_1_step_2_combined_datasets.py
@@ -87,5 +87,5 @@ def split_chunks(df,chunk_size,save_path,save_name):
   df_large.to_csv(os.path.join(save_path,f"{save_name}_large.csv"),index=False)
 
 chunk_size=300000
-split_chunks(ko_sequences_selected,chunk_size,os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/clustered"),"histidine_ko_no_blastp_no_rumhknet")
-split_chunks(blastp_sequences_selected,chunk_size,os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/clustered"),"histidine_blastp_ko_no_rumhknet")
+# split_chunks(ko_sequences_selected,chunk_size,os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/clustered"),"histidine_ko_no_blastp_no_rumhknet")
+# split_chunks(blastp_sequences_selected,chunk_size,os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/clustered"),"histidine_blastp_ko_no_rumhknet")
