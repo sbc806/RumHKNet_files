8fd8ca25284355b60f3f374186ba4d769f295340
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-20 14:54:14 -0700

    Update check_step_1_step_2_combined.py

--- predictions_files/step_1_step_2_combined/check_step_1_step_2_combined.py
+++ predictions_files/step_1_step_2_combined/check_step_1_step_2_combined.py
@@ -44,6 +44,7 @@ dataset_dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predicti
 clustered_contained=clustered_df["seq_id"].isin(method["seq_id"].values)
 new_seqs_contained=new_seqs_df["seq_id"].isin(method["seq_id"].values)
 print(len(method),np.sum(clustered_contained)+len(new_seqs_contained))
+print(clustered_df.shape,new_seqs_df.shape)
 print(np.unique(method["seq_id"].values).shape,np.unique(clustered_df["seq_id"].values).shape,np.unique(new_seqs_df["seq_id"].values).shape)
 print(np.unique(pd.concat([clustered_df["seq_id"],new_seqs_df["seq_id"]])["seq_id"]).shape)
 
