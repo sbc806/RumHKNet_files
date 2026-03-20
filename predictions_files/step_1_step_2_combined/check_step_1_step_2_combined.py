ec40398acde0a50320d566661d0c61a5f413e915
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-20 15:59:54 -0700

    Update check_step_1_step_2_combined.py

--- predictions_files/step_1_step_2_combined/check_step_1_step_2_combined.py
+++ predictions_files/step_1_step_2_combined/check_step_1_step_2_combined.py
@@ -43,11 +43,12 @@ method=pd.concat([rumhknet,ko,blastp])
 dataset_dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions"
 clustered_contained=clustered_df["seq_id"].isin(method["seq_id"].values)
 new_seqs_contained=new_seqs_df["seq_id"].isin(method["seq_id"].values)
+"""
 print(len(method),np.sum(clustered_contained)+np.sum(new_seqs_contained))
 print(clustered_df.shape,new_seqs_df.shape)
 print(np.unique(method["seq_id"].values).shape,np.unique(clustered_df["seq_id"].values).shape,np.unique(new_seqs_df["seq_id"].values).shape)
 print(np.unique(np.concatenate([clustered_df["seq_id"].values,new_seqs_df["seq_id"].values])).shape)
-
+"""
 clustered_df_remaining=clustered_df[~clustered_contained][["seq_id","seq"]]
 new_seqs_df_remaining=new_seqs_df[~new_seqs_contained][["seq_id","seq"]]
 chunk_size=2_450_000
