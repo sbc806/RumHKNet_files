65b8ba419cb70aa3cb1d4f162aedfad870179c1f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-26 23:47:38 -0700

    Update check_step_1_step_2_combined_predictions.py

--- predictions_files/step_1_step_2_combined/check_step_1_step_2_combined_predictions.py
+++ predictions_files/step_1_step_2_combined/check_step_1_step_2_combined_predictions.py
@@ -16,11 +16,14 @@ def check_specific(dir_path,f_name):
     dfs.append(df)
   return pd.concat(dfs)
 
-dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1_step_2_combined/both/clustered"
+dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1_step_2_combined/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1_step_2_combined/both/clustered"
 
-rumhknet=check_specific(dir_path,"histidine_rumhknet_predicted")
-ko=check_specific(dir_path,"histidine_ko_no_blastp_no_rumhknet")
-blastp=check_specific(dir_path,"histidine_blastp_ko_no_rumhknet")
+print("1508188 initial predictions")
+
+rumhknet=check_specific(predictions_path,"histidine_rumhknet_predicted")
+ko=check_specific(predictions_path,"histidine_ko_no_blastp_no_rumhknet")
+blastp=check_specific(predictions_path,"histidine_blastp_ko_no_rumhknet")
 
 print("RumHKNet:",len(rumhknet))
 print("KO:",len(ko))
@@ -29,3 +32,5 @@ print("Blastp:",len(blastp))
 print("RumHKNet histidine:",np.sum(rumhknet["prob"]>=0.1),np.sum(rumhknet["prob"]>=0.2),np.sum(rumhknet["pred"]))
 print("KO histidine:",np.sum(ko["prob"]>=0.1),np.sum(ko["prob"]>=0.2),np.sum(ko["pred"]))
 print("Blastp histidine:",np.sum(blastp["prob"]>=0.1),np.sum(blastp["prob"]>=0.2),np.sum(blastp["pred"]))
+
+print("Remaining sequences")
