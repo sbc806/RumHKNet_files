6b00401d5bcc0b707c0a6df8a54826d98e86405a
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-26 23:56:23 -0700

    Update check_step_1_step_2_combined_predictions.py

--- predictions_files/step_1_step_2_combined/check_step_1_step_2_combined_predictions.py
+++ predictions_files/step_1_step_2_combined/check_step_1_step_2_combined_predictions.py
@@ -41,6 +41,9 @@ total_predicted_kinases=0
 for i in selected_i:
   df_i=check_speicific(predictions_path,"unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_small")
   i_df[i]=df_i
+  print(i,"Number of predicted kinases:",np.sum(df_i["prob"]>=0.1),np.sum(df_i["prob"]>=0.2),np.sum(df_i["pred"]))
+  total_predicted_kinases=total_predicted_kinases+np.sum(df_i["pred"])
+  
 large=pd.read_csv(os.path.join(dataset_path,"unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_large.csv"))
 large_predicted=pd.read_csv(os.path.join(predictions_path,"unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_large_predicted_02_v2.csv"))
 print(len(large),len(large_predicted))
