0ab95c633b3f2f9c880ffefb2f5d2bc8ca050363
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-30 12:28:19 -0700

    Update cluster_data_prediction_csv.py

--- predictions_files/cluster_data/cluster_data_prediction_csv.py
+++ predictions_files/cluster_data/cluster_data_prediction_csv.py
@@ -12,5 +12,5 @@ for i, fasta in enumerate(fasta_content):
   seq_data.append({"seq_id":seq_id,"seq":seq})
 
 seq_data_df=pd.DataFrame(seq_data)
-print(seq_data)
+print(seq_data_df)
 seq_data_df.to_csv(os.path.join(dir_path,"newadd_155098MAGs.csv"),index=False)
