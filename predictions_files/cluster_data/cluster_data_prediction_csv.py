e46d713b72a749a95d546f70bfb65d9299b08519
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-30 13:35:29 -0700

    Update cluster_data_prediction_csv.py

--- predictions_files/cluster_data/cluster_data_prediction_csv.py
+++ predictions_files/cluster_data/cluster_data_prediction_csv.py
@@ -16,46 +16,7 @@ print(seq_data_df)
 seq_data_df.to_csv(os.path.join(dir_path,"newadd_155098MAGs.csv"),index=False)
 """
 seq_data_df=pd.read_csv(os.path.join(dir_path,"newadd_155098MAGs.csv"))
-import numpy as np
-import os as os
-import pandas as pd
 
-dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
-
-histidine_02=pd.read_csv(os.path.join(dir_path,"RumHKNet_csv/step_1_02_step_2_02/step_3_clustered_newrun_rbags_predicted_02.csv"))
-
-ko=pd.read_csv(os.path.join(dir_path,"histidine_other_software/total_KO_95%.txt"),header=None)
-blastp=pd.read_csv(os.path.join(dir_path,"histidine_other_software/final_Blastp_HK95%_3050100.txt"),header=None)
-
-print("Number of histidine kinases predicted by RumHKNet:",len(histidine_02),np.unique(histidine_02["seq_id"]).shape)
-print("Number of histidine kinases predicted by KO:",len(ko),np.unique(ko[0]).shape)
-print("Number of histidine kinases predicted by Blastp:",len(blastp),np.unique(blastp[0]).shape)
-print(histidine_02[["seq_id","seq"]])
-# histidine_02[["seq_id","seq"]].to_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/histidine_rumhknet_predicted_02_02.csv"),index=None)
-histidine_02_small=histidine_02[histidine_02["seq"].str.len()<=1500][["seq_id","seq"]]
-histidine_02_large=histidine_02[histidine_02["seq"].str.len()>1500][["seq_id","seq"]]
-# print(histidine_02_small)
-# print(histidine_02_large)
-# histidine_02_small.to_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/clustered/histidine_rumhknet_predicted_02_02_small.csv"),index=False)
-# histidine_02_large.to_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1_step_2_combined/clustered/histidine_rumhknet_predicted_02_02_large.csv"),index=False)
-
-ko_rumhknet=ko[0].isin(histidine_02["seq_id"])
-blastp_rumhknet=blastp[0].isin(histidine_02["seq_id"])
-
-print("RumHKNet and KO:",np.sum(ko_rumhknet))
-print("RumHKNet and Blastp:",np.sum(blastp_rumhknet))
-
-ko_blastp=ko[0].isin(blastp[0])
-print("KO and Blastp:",np.sum(ko_blastp))
-
-ko_only=ko[~ko_rumhknet&~ko_blastp]
-print("KO only:",len(ko_only))
-
-blastp_selected=blastp[~blastp_rumhknet]
-print("Blastp and not RumHKNet:",len(blastp_selected))
-
-print(np.unique(np.hstack((histidine_02["seq_id"].values,ko[0].values,blastp[0].values))).shape)
-print(np.unique(np.hstack((histidine_02["seq_id"].values,ko_only[0].values,blastp_selected[0].values))).shape)
 def split_chunks(df,chunk_size,save_path,save_name):
   small=df["seq"].str.len()<=1500
   large=df["seq"].str.len()>1500
