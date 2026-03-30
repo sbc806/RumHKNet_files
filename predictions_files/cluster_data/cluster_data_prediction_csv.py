e359db8188b9ed6c5561bdd1cf039a76f6b253d2
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-30 12:41:29 -0700

    Update cluster_data_prediction_csv.py

--- predictions_files/cluster_data/cluster_data_prediction_csv.py
+++ predictions_files/cluster_data/cluster_data_prediction_csv.py
@@ -5,7 +5,7 @@ import pandas as pd
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/cluster_data"
 fasta_path=os.path.join(dir_path,"newadd_155098MAGs.fasta")
 
-fasta_content=SeqIO.parse(open(dir_path),"fasta")
+fasta_content=SeqIO.parse(open(fasta_path),"fasta")
 seq_data=[]
 for i, fasta in enumerate(fasta_content):
   seq_id,seq=fasta.id,fasta.seq
