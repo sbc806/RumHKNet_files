c08f34695394a13b4e3ece03943d48db799e2b44
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-04 20:42:23 -0700

    Update step_1_results.py

--- predictions_files/cluster_data/step_1_results.py
+++ predictions_files/cluster_data/step_1_results.py
@@ -2,6 +2,12 @@ import os
 import pandas as pd
 import numpy as np
 
-dataset_path = "/home/schen123/proejcts/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step-1/clustered"
+dataset_path = "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered"
 predictions_path = "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
 
+i_df={}
+for i in range(0,5):
+  df_i=get_select(predictions_path,"newadd_155098MAGs_small_{i}")
+  print(len(df_i))
+  i_df[i]=df_i
+large_df=pd.read_csv(os.path.join("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/newadd_155098MAGs_large_predicted_02_v2.csv"))
