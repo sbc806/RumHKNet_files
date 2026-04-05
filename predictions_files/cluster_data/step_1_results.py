c411e40863c5fb7d1a15d957387e48f65bc2158c
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-04 20:38:22 -0700

    Update step_1_results.py

---
+++ predictions_files/cluster_data/step_1_results.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+import numpy as np
+
+dataset_path = "/home/schen123/proejcts/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step-1/clustered"
+predictions_path = "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
+
