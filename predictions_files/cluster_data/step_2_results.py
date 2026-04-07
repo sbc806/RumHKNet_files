5f968e6e1b256e2920753e0dcafaff955d232ca7
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-06 21:32:11 -0700

    Update step_2_results.py

--- predictions_files/cluster_data/step_2_results.py
+++ predictions_files/cluster_data/step_2_results.py
@@ -5,5 +5,5 @@ import pandas as pd
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/predictions/predictions_dataset/step_2/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/predictions/predicted_results/step_2/both/clustered"
 
-large=pd.read_csv(os.path.join(predicions_path,"newadd_155098MAGs_large_step_1_kinase_predicted_02_v2.csv"))
+large=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_1_kinase_predicted_02_v2.csv"))
 print(large)
