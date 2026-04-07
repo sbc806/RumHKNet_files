39e5b8ad0e67e1bbff8b4f2baf2573f96944429c
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-06 21:35:13 -0700

    Update step_2_results.py

--- predictions_files/cluster_data/step_2_results.py
+++ predictions_files/cluster_data/step_2_results.py
@@ -7,3 +7,5 @@ predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predicti
 
 large=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_1_kinase_predicted_02_v2.csv"))
 print(large)
+large_histidine=large["label"]==1
+print("large",np.sum(large["prob"]>=0.2),np.sum(large_histidine))
