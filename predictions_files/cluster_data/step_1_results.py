5a914c6b71b78273fc0db7fbdbbb069d52ef9914
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-07 16:22:39 -0700

    Update step_1_results.py

--- predictions_files/cluster_data/step_1_results.py
+++ predictions_files/cluster_data/step_1_results.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 sys.path.append("..")
 from predictions_helpers import check_specific
-
+from predictions_helpers import split_chunks
 
 dataset_path = "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered"
 predictions_path = "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
