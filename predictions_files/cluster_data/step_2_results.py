866da5a7085ab1fb18a71933aaf96d295761b71a
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 16:00:07 -0700

    Update step_2_results.py

--- predictions_files/cluster_data/step_2_results.py
+++ predictions_files/cluster_data/step_2_results.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_2/both/clustered"
-
+i_df={}
+for i in range(0,4):
+  df_i=
 large=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_1_kinase_predicted_02_v2.csv"))
 print(large)
 large_histidine=large["label"]==1
