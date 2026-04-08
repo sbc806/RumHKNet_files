553d80abe6ec988c854eae06375602c9397381aa
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 15:41:20 -0700

    Update step_1_results.py

--- predictions_files/cluster_data/step_1_results.py
+++ predictions_files/cluster_data/step_1_results.py
@@ -25,18 +25,18 @@ for i in i_df:
   kinase_dfs.append(df_i[df_i["pred"]==1])
 
 kinase_small_df=pd.concat(kinase_dfs)[["seq_id","seq"]]
-print(len(kinase_small_df))
+print("Number of predicted kinases for sequences <= 1500:",len(kinase_small_df))
 print(kinase_small_df)
 chunk_size=200000
 # split_chunks(kinase_small_df,chunk_size,"/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered","newadd_155098MAGs_step_1_kinase")
 # kinase_small_df.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/newadd_155098MAGs_small_step_1_kinase.csv",index=False)
 
 large_df=pd.read_csv(os.path.join("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/newadd_155098MAGs_large_predicted_02_v2.csv"))
-print(len(large_df))
+print("Number of predictions for sequences > 1500:",len(large_df))
 large_df.columns=i_df[0].columns
 print(np.sum(large_df["prob"]>=0.2),np.sum(large_df["pred"]))
 
 kinase_large_df=large_df[large_df["pred"]==1][["seq_id","seq"]]
-print(len(kinase_large_df))
+print("Number of predicted kinases for sequences > 1500:",len(kinase_large_df))
 print(kinase_large_df)
 # kinase_large_df.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/newadd_155098MAGs_large_step_1_kinase.csv",index=False)
