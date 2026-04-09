0dd62de726e8c394b08ce07066674bb88787b459
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 22:22:52 -0700

    Update run_step_1_results.sh

---
+++ predictions_files/cluster_data/run_step_1_results.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --mem=100G
+#SBATCH --time=7-0
+#SBATCH --job-name=both-step-1-no-matrix-dirpath
+#SBATCH --output=/scratch/schen123/kinases/RumHKNet_output/step_1/both/both_step_1_no_matrix_dirpath_%j.out
+#SBATCH --err=/scratch/schen123/kinases/RumHKNet_output/step_1/both/both_step_1_no_matrix_dirpath_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/both
+cat both_step_1_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/RumHKNet_output/step_1/both/both_step_1_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./both_step_1_no_matrix_dirpath.sh
+
+
+deactivate
