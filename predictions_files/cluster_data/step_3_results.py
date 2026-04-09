18377f0f728705f6323dccaae154d28c42c12868
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 21:32:39 -0700

    Update step_3_results.py

--- predictions_files/cluster_data/step_3_results.py
+++ predictions_files/cluster_data/step_3_results.py
@@ -5,5 +5,7 @@ import sys
 sys.path.append("..")
 from predictions_helpers import check_specific, df_to_fasta
 
-dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
+dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_3/both/clustered"
+
 
