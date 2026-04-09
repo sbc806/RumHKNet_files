c77a0eedd794d9c0dab5c52f7750a0d7c1094d97
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 21:37:22 -0700

    Update step_3_results.py

--- predictions_files/cluster_data/step_3_results.py
+++ predictions_files/cluster_data/step_3_results.py
@@ -9,4 +9,5 @@ dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_3/both/clustered"
 
 large_step_3=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_2_histidine_kinase_predicted_02.csv"))
+print(large_step_3.columns)
 print(large_step_3)
