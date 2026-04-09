859da4a14998e39f3e163b5c41fca8d7c5563396
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 21:48:48 -0700

    Update step_3_results.py

--- predictions_files/cluster_data/step_3_results.py
+++ predictions_files/cluster_data/step_3_results.py
@@ -15,5 +15,5 @@ print(large_step_3)
 
 large_step_3_selected=large_step_3[["seq_id","seq","top1_label"]]
 large_step_3_selected.columns=["seq_id","seq","batch"]
-large_step_3_selected[np.where(large_step_3_selected["batch"]==10)[0],2]=-1
+large_step_3_selected.iloc[np.where(large_step_3_selected["batch"]==10)[0],2]=-1
 print(large_step_3_selected)
