107d2051e3525aa92120019390d9a7f5e9835f4b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-08 21:36:38 -0700

    Update step_3_results.py

--- predictions_files/cluster_data/step_3_results.py
+++ predictions_files/cluster_data/step_3_results.py
@@ -8,5 +8,5 @@ from predictions_helpers import check_specific, df_to_fasta
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_3/both/clustered"
 
-large_step_3=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_2_histidine_kinase.csv"))
+large_step_3=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_2_histidine_kinase_predicted_02.csv"))
 print(large_step_3)
