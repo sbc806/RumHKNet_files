1815ec6962c8076922a369446f26b3f891353c81
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 10:53:36 -0700

    Update step_4_results.py

--- predictions_files/cluster_data/step_4_results.py
+++ predictions_files/cluster_data/step_4_results.py
@@ -26,3 +26,4 @@ large_selected.columns=["seq_id","seq","pred"]
 step_4=pd.concat(smal_selected,large_selected])
 with open("/home/schen123/scratch/kinases/kinases_dataset/extra_p_133_v3_batch/protein/multi_class/label.json","r") as f:
   ko_label=json.load(f)
+print(ko_label)
