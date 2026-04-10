8285213cd9cb4a686ce06b2964f1b8414d3fb3fe
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-09 20:14:38 -0700

    Update step_3_results.py

--- predictions_files/cluster_data/step_3_results.py
+++ predictions_files/cluster_data/step_3_results.py
@@ -17,7 +17,7 @@ small_step_3_selected=small_step_3[["seq_id","seq","top1_label"]]
 small_step_3_selected.columns=["seq_id","seq","batch"]
 small_step_3_selected.iloc[np.where(small_step_3_selected["batch"]==10)[0],2]=-1
 print(small_step_3_selected)
-
+small_step_3_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered/newadd_155098MAGs_small_step_3_histidine_kinase_family.csv",index=False)
 large_step_3=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_2_histidine_kinase_predicted_02.csv"))
 print(large_step_3.columns)
 print(large_step_3["top1_label"])
