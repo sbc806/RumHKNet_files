02cd8ed0a06691c2d7778658fd87451c127b0bba
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 13:38:31 -0700

    Update 5_isolate_csv.py

--- predictions_files/5_isolate/5_isolate_csv.py
+++ predictions_files/5_isolate/5_isolate_csv.py
@@ -3,4 +3,10 @@ import os
 import numpy as np
 import pandas as pd
 import sys
+sys.path.append("..")
+from predictions_helpers import fasta_to_df
+
+
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
+
+isolate=fasta_to_df
