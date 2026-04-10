0e7d26f759abb9764bac9f1c59c74a0cb1aa66f0
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 13:43:25 -0700

    Update 5_isolate_csv.py

--- predictions_files/5_isolate/5_isolate_csv.py
+++ predictions_files/5_isolate/5_isolate_csv.py
@@ -12,3 +12,11 @@ dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 isolate=fasta_to_df(os.path.join(dir_path,"5_isolate.faa"))
 print(isolate)
 print(np.unique(isolate["seq_id"]).shape,np.unique(isolate["seq"]).shape)
+
+small=isolate["seq"].str.len()<=1500
+large=isolate["se"].str.len()>1500
+print("Number of sequences <= 1500:",np.sum(small))
+print("Number of sequences > 1500:",np.sum(large))
+
+isolate[small].to_csv(os.path.join(dir_path,"predictions/predictions_datasest/step_1/clustered/5_isolate_small.csv"),index=False)
+isolate[
