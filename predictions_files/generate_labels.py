048479d12cffaa106d773bd6a806577febb438cf
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-09 20:46:16 -0700

    Update generate_labels.py

--- predictions_files/generate_labels.py
+++ predictions_files/generate_labels.py
@@ -12,14 +12,6 @@ def make_df(dir_path):
     dfs.append(df)
   return pd.concat(dfs)
 
-print("clustered_rep_seq95")
-dataset_path="../../predictions/predictions_dataset/step_3/clustered"
-predictions_path="../../predictions/predicted_results/step_3/both/clustered"
-small_histidine_df=make_df(os.path.join(predictions_path,"small_histidine_kinase"))
-predictions_information(small_histidine_df)
-
-large_histidine_df=pd.read_csv(os.path.join(predictions_path,"clustered_rep_seq95_large_histidine_kinase_predicted_03.csv"))
-predictions_information(large_histidine_df)
 
 # Want to piar family to KO
 # TGet full dataset of histidne kinases used for training, train, dev, and test
