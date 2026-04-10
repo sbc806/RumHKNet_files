1d3eb0c78288c008ff2629472edbc01aa58183d1
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 10:45:37 -0700

    Update step_4_results.py

--- predictions_files/cluster_data/step_4_results.py
+++ predictions_files/cluster_data/step_4_results.py
@@ -6,4 +6,15 @@ import sys
 sys.path.append("..")
 from predictions_helpers import reverse_dict, df_to_fasta, add_label
 
+dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_4/both/clustered"
 
+small=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_small_step_3_histidine_kinase_family_predicted_02.csv"))
+print(small)
+print(small.columns)
+
+print()
+
+large=pd.read_csv(os.path.join(predictions_path,"newadd_155098MAGs_large_step_3_histidine_kinase_family_predicted_02.csv"))
+print(large)
+print(large.columns)
