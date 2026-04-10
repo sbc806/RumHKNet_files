f66f753ca0fc48504037ee9da43b4e24d8de93a8
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-09 19:48:16 -0700

    Update predictions_helpers.py

--- predictions_files/predictions_helpers.py
+++ predictions_files/predictions_helpers.py
@@ -50,8 +50,8 @@ def fasta_to_df(fasta_path):
   return pd.DataFrame(all_fasta)
 
 def df_to_fasta(df,save_path):
-  for i in range(0,len(df)):
-    with open(save_path,"w") as fasta_file:
+  with open(save_path,"w") as fasta_file:
+    for i in range(0,len(df)):
       seq_id=df["seq_id"].iloc[i]
       seq=df["seq"].iloc[i]
       fasta_file.write(f">{seq_id}\n")
