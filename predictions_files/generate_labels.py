7c57d875f53cb69848614bf6c2bd08187708279f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-09 20:54:06 -0700

    Update generate_labels.py

--- predictions_files/generate_labels.py
+++ predictions_files/generate_labels.py
@@ -29,8 +29,8 @@ test_df=pd.read_csv("/home/schen123/scratch/kinases/kinases_dataset/extra_p_133_
 histidine_full_df=pd.concat([train_df,dev_df,test_df])
 print("Number of histidine kinases:",len(histidine_full_df))
 
-step_3_train_df=pd.read_csv("../../sbc806/RumHKNet/kinases_dataset/step_3_11_family/protein/multi_class/train/train.csv")
-step_3_dev_df=pd.read_csv("../../sbc806/RumHKNet/kinases_dataset/step_3_11_family/protein/multi_class/dev/dev.csv")
+step_3_train_df=pd.read_csv("/home/schen123/scratch/kinases/kinases_dataset/step_3_11_family/protein/multi_class/train/train.csv")
+step_3_dev_df=pd.read_csv("/home/schen123/scratch/kinases/kinases_dataset/step_3_11_family/protein/multi_class/dev/dev.csv")
 step_3_test_df=pd.read_csv("../../sbc806/RumHKNet/kinases_dataset/step_3_11_family/protein/multi_class/test/test.csv")
 step_3_full_df=pd.concat([step_3_train_df,step_3_dev_df,step_3_test_df])
 print("Number of histidine kinases for step 3:",len(step_3_full_df))
