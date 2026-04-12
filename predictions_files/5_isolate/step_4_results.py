81d2c887a6de9a81a300bc8f0d179f1c23bbdf30
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 07:14:08 -0700

    Update step_4_results.py

--- predictions_files/5_isolate/step_4_results.py
+++ predictions_files/5_isolate/step_4_results.py
@@ -2,6 +2,10 @@ import json
 import os
 import numpy as np
 import pandas as pd
+import sys
+sys.path.append("..")
+from predictions_helpers import add_label, reverse_dict, dft_to_fasta
+
 
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_4/both/clustered"
@@ -13,7 +17,11 @@ small_selected=small[["seq_id","seq","top1_label"]]
 small_selected.columns=["seq_id","seq","pred"]
 print(small_selected)
 
-with open("/home/schen23/scratch/kinases/kinases_dataset/extra_p_133_class_v3/protein/multi_class/label.json","r") ads fL:
+with open("/home/schen23/scratch/kinases/kinases_dataset/extra_p_133_class_v3/protein/multi_class/label.json","r") as f:
   label_ko=json.load(f)
-small_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predic5_isolate_predictions/RumHKnet_predictions/step_1_02_step_2_02/5_isolate_step_4_histidine_kinase_ko.csv")
+
+small_selected=add_label(small_selected,reverse_dict(label_ko))
+print(small_selected)
+
+small_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predic5_isolate_predictions/RumHKNet_predictions/step_1_02_step_2_02/5_isolate_step_4_histidine_kinase_ko.csv",index=False)
 df_to_fasta(small_selected,"/home/schen123/projects/rrg-guanuofa/schen123/kinases/5_isolate_predictions/RumHKNet_predictions/step_1_02_step_2_02/5_isolate_step_4_histidine_kinase_ko.fasta",extra_column="pred_other")
