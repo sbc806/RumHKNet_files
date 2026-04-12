360cdc588959f9ac766182cb9ea33e6c205bcfac
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:10:58 -0700

    Update step_3_results.py

--- predictions_files/5_isolate/step_3_results.py
+++ predictions_files/5_isolate/step_3_results.py
@@ -16,4 +16,4 @@ small_selected.columns=["seq_id","seq","batch"]
 small_selected.iloc[np.where(small_selected["batch"]==10)[0],2]=-1
 print(small_selected)
 
-small_selected.to_csv(?"/home/schen123/projects/rrg-guanuofa")
+small_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered",index=False)
