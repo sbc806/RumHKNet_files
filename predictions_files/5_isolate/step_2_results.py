fe722e5ac8ccb97f164c352273f919a2eed341fa
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 00:50:27 -0700

    Update step_2_results.py

--- predictions_files/5_isolate/step_2_results.py
+++ predictions_files/5_isolate/step_2_results.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
-predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_2/both/clusterd"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_2/both/clustered"
 
 small=pd.read_csv(os.path.join(predictions_path,"5_isolate_step_1_kinase_small_predicted_02_v2.csv"))
 print(len(small))
