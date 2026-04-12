ef1eb9a30112185e92412857342ba9cc6b51b3d4
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:09:18 -0700

    Update step_3_results.py

--- predictions_files/5_isolate/step_3_results.py
+++ predictions_files/5_isolate/step_3_results.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 
-dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered"
-predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_4/both/clustered"
+dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_3/both/clustered"
 
 small=pd.read_csv(os.path.join(predictions_path,"5_isolate_step_2_histidine_kinase_small_predicted_02_v2.csv"))
 print(len(small))
