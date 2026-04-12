1ea18694a17df35670b6d46c521d333a30b6648f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:05:29 -0700

    Update step_3_results.py

---
+++ predictions_files/5_isolate/step_3_results.py
@@ -0,0 +1,15 @@
+import os
+impotr numpy as np
+import pandas aspd
+
+dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_4/both/clustered"
+
+small=pd.read_csv(os.path.join(predictions_path,"5_isolate_step_2_histidine_kinase_small_predicted_02_v2.csv"))
+print(len(small))
+
+print(small)
+print(small.columns)
+
+small_selected=small[["seq_id","seq","top1_label"]]
+small_selected.iloc[]=-1
