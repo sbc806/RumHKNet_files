337eac892b00b5d644d7361a9bf0ed4459a08391
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 07:02:41 -0700

    Update step_3_results.py

--- predictions_files/5_isolate/step_3_results.py
+++ predictions_files/5_isolate/step_3_results.py
@@ -1,3 +1,4 @@
+import json
 import os
 import numpy as np
 import pandas as pd
@@ -21,3 +22,8 @@ small_selected.iloc[np.where(small_selected["batch"]==10)[0],2]=-1
 print(small_selected)
 
 # small_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered/5_isolate_step_3_histidine_kinase_family_small.csv",index=False)
+
+with open("/home/schen123/scratch/kinases/kinases_dataset/step_3_11_family/protein/multi_class/label.json","r") as f:
+  label_family=json.load(f)
+small_selected=add_label(small_selected,reverse_dict(label_family))
+print(small_selected)
