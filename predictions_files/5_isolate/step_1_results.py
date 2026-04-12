8d0461c9ceaa55e7b56c7cfb86961eca00407065
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:42:49 -0700

    Update step_1_results.py

--- predictions_files/5_isolate/step_1_results.py
+++ predictions_files/5_isolate/step_1_results.py
@@ -17,7 +17,7 @@ print(np.mean(small_v3["prob"]-small_v2["prob"]))
 print(np.mean(round(small_v3["prob"],5)-round(small_v2["prob"],5)))
 print(np.sum(small_v2["label"].values==small_v3["pred"].values),np.sum(small_v2["label"].values!=small_v3["pred"].values))
 
-small_v2[small_v2["label"]==1][["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/5_isolate_step_1_kinase_small.csv",index=False)
+# small_v2[small_v2["label"]==1][["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/5_isolate_step_1_kinase_small.csv",index=False)
 
 print(np.sum(small_v2["prob"]>=0.2),np.sum(small_v2["label"]==1))
 print(np.sum(small_v3["prob"]>=0.2),np.sum(small_v3["pred"]==1))
@@ -25,3 +25,5 @@ print(np.sum(small_v3["prob"]>=0.2),np.sum(small_v3["pred"]==1))
 large=pd.read_csv(os.path.join(predictions_path,"5_isolate_large_predicted_02_v2.csv"))
 print(len(large))
 print(np.sum(large["prob"]>=0.2),np.sum(large["label"]==1))
+
+small_v2[small_v2["label"]==1][["seq_id","seq"]].to_csv("
