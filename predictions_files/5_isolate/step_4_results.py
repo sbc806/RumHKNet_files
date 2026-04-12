abfd93be8caed50b368be2bc53be7ecd137d3941
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:41:03 -0700

    Update step_4_results.py

--- predictions_files/5_isolate/step_4_results.py
+++ predictions_files/5_isolate/step_4_results.py
@@ -9,4 +9,8 @@ small=pd.read_csv(os.path.join(predictions_path,"5_isolate_step_3_histidine_kina
 print(small)
 
 small_selected=small[["seq_id","seq","top1_label"]]
-small
+small_selected.columns=["seq_id","seq","pred"]
+print(small_selected)
+
+small_selected.to_csv(os."/home/schen123/projects/rrg-guanuofa/schen123/kinases/predic5_isolate_predictions/RumHKnet_predictions/step_1_02_step_2_02/5_isolate_step_4_histidine_kinase_ko.csv")
+df_to_fasta(small_selected,"/home/schen123/projects/rrg-guanuofa/schen123/kinases/5_isolate_predictions/RumHKNet_predictions/step_1_02_step_2_02/5_isolate_step_4_histidine_kinase_ko.fasta"extra)
