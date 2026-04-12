c7de4e97b80f98d6cb210f244b34698e0e212862
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:53:43 -0700

    Update step_3_results.py

--- predictions_files/5_isolate/step_3_results.py
+++ predictions_files/5_isolate/step_3_results.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pandas as pd
+import sys
+sys.path.append("..")
 
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_3/both/clustered"
