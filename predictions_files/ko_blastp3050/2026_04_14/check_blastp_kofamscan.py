8b7c9c5ea19d4f89a74b4e3b20ca00903d13b529
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-14 22:29:40 -0700

    Update check_blastp_kofamscan.py

--- predictions_files/ko_blastp3050/2026_04_14/check_blastp_kofamscan.py
+++ predictions_files/ko_blastp3050/2026_04_14/check_blastp_kofamscan.py
@@ -1,3 +1,12 @@
 import os
 import numpy as np
 import pandas as pd
+
+dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/2026_04_14_blastp_kofamscan/cluster_data"
+
+blastp=pd.read_csv(os.path.join(dir_path,"blastp_HK_cluster95.txt"),header=None)
+kofamscan=pd.read_csv(os.path.join(dir_path,"kofam_HK_cluster95.txt"),header=None)
+
+print(len(blastp),len(kofamscan))
+
+print(np.unique(blastp[0]).shape,np.unique(kofamscan[0]).shape)
