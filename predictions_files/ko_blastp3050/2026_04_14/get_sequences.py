a542e16f5c6345baf79cff6c208c6311a0cc7f4c
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-14 23:00:30 -0700

    Update get_sequences.py

--- predictions_files/ko_blastp3050/2026_04_14/get_sequences.py
+++ predictions_files/ko_blastp3050/2026_04_14/get_sequences.py
@@ -5,8 +5,8 @@ import pandas as pd
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 blastp_kofamscan_path=os.path.join(dir_path,"2026_04_14_blastp_kofamscan/cluster_data")
 
-blastp=pd.read_csv(os.path.join(blastp_kofamscan_dir_path,"blastp_HK_cluster95.txt"),header=None)
-kofamscan=pd.read_csv(os.path.join(blastp_kofamscan_dir_path,"kofam_HK_cluster95.txt"),header=None)
+blastp=pd.read_csv(os.path.join(blastp_kofamscan_path,"blastp_HK_cluster95.txt"),header=None)
+kofamscan=pd.read_csv(os.path.join(blastp_kofamscan_path,"kofam_HK_cluster95.txt"),header=None)
 
 print(len(blastp),len(kofamscan))
 
