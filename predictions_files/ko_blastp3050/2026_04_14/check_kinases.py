b0923466f1e94ae0119b3c6dca8a67d58d74e65a
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-15 00:04:29 -0700

    Update check_kinases.py

--- predictions_files/ko_blastp3050/2026_04_14/check_kinases.py
+++ predictions_files/ko_blastp3050/2026_04_14/check_kinases.py
@@ -7,7 +7,7 @@ dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 blastp_kofamscan_path=os.path.join(dir_path,"2026_04_14_blastp_kofamscan/cluster_data")
 
 def get_df(dir_path,desired_file=""):
-  chosen=[f for f in dir_path if desired_file in f and "sequences" in f]
+  chosen=[f for f in os.listdir(dir_path) if desired_file in f and "sequences" in f]
   dfs=[]
   for f in chosen:
     df=pd.read_csv(os.path.join(dir_path,f))
