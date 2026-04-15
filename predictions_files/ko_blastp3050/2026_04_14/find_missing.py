e9cddbce701027ab2d634279c732a49a70004308
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-15 01:08:02 -0700

    Update find_missing.py

--- predictions_files/ko_blastp3050/2026_04_14/find_missing.py
+++ predictions_files/ko_blastp3050/2026_04_14/find_missing.py
@@ -6,8 +6,11 @@ dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 
 # clustered=pd.read_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95.csv"))
 # print(len(clustered))
-newrun=pd.read_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1/clustered/newrun_seqs.csv"))
-print(len(newrun))
+# newrun=pd.read_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1/clustered/newrun_seqs.csv"))
+# print(len(newrun))
+unique=pd.read_csv(os.path.join(dir_path,"unique_clustered_rep_seq_All140086RBAGs_95_90.csv"))
+print(len(unique))
+print(unique)
 
 blastp_missing=pd.read_csv("blastp_missing.csv",header=None)
 kofamscan_missing=pd.read_csv("kofamscan_missing.csv",header=None)
@@ -16,5 +19,10 @@ print(len(blastp_missing),len(kofamscan_missing))
 # print(np.sum(blastp_missing.isin(clustered["seq_id"].values)))
 # print(np.sum(kofamscan_missing.isin(clustered["seq_id"].values)))
 
-print(np.sum(blastp_missing.isin(newrun["seq_id"].values)))
-print(np.sum(kofamscan_missing.isin(newrun["seq_id"].values)))
+# print(np.sum(blastp_missing.isin(newrun["seq_id"].values)))
+# print(np.sum(kofamscan_missing.isin(newrun["seq_id"].values)))
+
+blastp_contained=blastp_missing.isin(unique["seq_id"].values)
+kofamscan_contained=kofamscan_missing.isin(unique["seq_id"].values)
+print(np.sum(blastp_contained))
+print(np.sum(kofamscan_contained))
