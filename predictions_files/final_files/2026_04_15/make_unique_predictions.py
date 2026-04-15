f400007e011f28c0426fb2de72286bdeaff2cde5
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-15 16:24:42 -0700

    Update make_unique_predictions.py

--- predictions_files/final_files/2026_04_15/make_unique_predictions.py
+++ predictions_files/final_files/2026_04_15/make_unique_predictions.py
@@ -8,4 +8,22 @@ rumhknet_path=os.path.join(dir_path,"RumHKNet_csv/step_1_02_step_2_02")
 rumhknet=pd.read_csv(os.path.join(rumhknet_path,"step_1_clustered_newrun_rbags_predicted_02.csv"))
 print(len(rumhknet))
 
-unique=pd.read_csv(os.path.join(dir_path,"unique_clustered_rep"))
+unique=pd.read_csv(os.path.join(dir_path,"unique_clustered_rep_seq_All140086RBAGs_95_90.csv"))
+print(len(unique))
+
+def get_information(df):
+  print("Number of unique sequence IDs:",np.unique(df["seq_id"]).shape)
+  print("Number of unique sequences:",np.unique(df["seq"]).shape)
+
+get_information(rumhknet)
+print()
+get_information(unique)
+
+print()
+
+contained=unique["seq_id"].isin(rumhknet["seq_id"].values)
+print(np.sum(contained),np.sum(~contained))
+
+selected=rumhknet["seq_id"].isin(unique["seq_id"].values)
+print(np.sum(selected)
+rumhknet[selected].to_csv(os.path.join(dir_path,"unique_clustered_rep_seq_All140086RBAGs_95_90_step_1_predictions_02.csv"),index=False)
