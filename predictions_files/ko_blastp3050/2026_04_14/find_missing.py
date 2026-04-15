fc49351ee9379f9ad6485fed4cddda1f0431a8c3
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-15 12:24:26 -0700

    Update find_missing.py

--- predictions_files/ko_blastp3050/2026_04_14/find_missing.py
+++ predictions_files/ko_blastp3050/2026_04_14/find_missing.py
@@ -29,10 +29,17 @@ kofamscan_missing=pd.read_csv("kofamscan_missing_replaced.csv",header=None)
 print(len(blastp_missing),len(kofamscan_missing))
 print(blastp_missing,kofamscan_missing)
 all_missing=[blastp_missing,kofamscan_missing]
+method_missing={"blastp":blastp_missing,"kofamscan":kofamscan_missing}
 
 def get_contained_information(df,missing):
   for each_missing in missing:
     print(np.sum(each_missing.isin(df["seq_id"].values)))
+
+def get_sequences(df,method_missing):
+  for method in method_missing:
+    df_contained=df[df["seq_id"].isin(method_missing[0])]
+    print(method,len(contained))
+    df_contained.to_csv(os.path.join(dir_path,f"2026_04_14/cluster_data/unique_{method}_missing_sequences.csv"))
     
 # print(np.sum(blastp_missing.isin(clustered["seq_id"].values)))
 # print(np.sum(kofamscan_missing.isin(clustered["seq_id"].values)))
@@ -40,11 +47,11 @@ def get_contained_information(df,missing):
 # print(np.sum(blastp_missing.isin(newrun["seq_id"].values)))
 # print(np.sum(kofamscan_missing.isin(newrun["seq_id"].values)))
 
-# blastp_contained=blastp_missing.isin(unique["seq_id"].values)
-# kofamscan_contained=kofamscan_missing.isin(unique["seq_id"].values)
-# print(np.sum(blastp_contained))
-# print(np.sum(kofamscan_contained))
+blastp_contained=blastp_missing.isin(unique["seq_id"].values)
+kofamscan_contained=kofamscan_missing.isin(unique["seq_id"].values)
+print("Blastp:",np.sum(blastp_contained),np.sum(~blastp_contained))
+print("Kofamscan:",np.sum(kofamscan_contained),np.sum(~kofamscan_contained))
 
-get_contained_information(newadd,all_missing)
-print()
-get_contained_information(isolate,all_missing)
+# get_contained_information(newadd,all_missing)
+# print()
+# get_contained_information(isolate,all_missing)
