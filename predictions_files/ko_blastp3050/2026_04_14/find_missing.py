2e1da916ac380d7f255fcd1c3daed27baf2870d8
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-15 00:53:22 -0700

    Update find_missing.py

--- predictions_files/ko_blastp3050/2026_04_14/find_missing.py
+++ predictions_files/ko_blastp3050/2026_04_14/find_missing.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 
-clustered=pd.read_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95.csv"))
-print(len(clustered))
+# clustered=pd.read_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1/clustered/clustered_rep_seq95.csv"))
+# print(len(clustered))
 newrun=d.read_csv(os.path.join(dir_path,"predictions/predictions_dataset/step_1/clustered/newrun_seqs.csv"))
+print(len(newrun))
 
 blastp_missing=pd.read_csv("blastp_missing.csv",header=None)
 kofamscan_missing=pd.read_csv("kofamscan_missing.csv",header=None)
