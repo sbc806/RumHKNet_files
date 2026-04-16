2ed982728675c9a87d93e14df7813034c71b7b51
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-15 20:47:41 -0700

    Update check_histidine.py

--- predictions_files/ko_blastp3050/2026_04_14/5_isolate/check_histidine.py
+++ predictions_files/ko_blastp3050/2026_04_14/5_isolate/check_histidine.py
@@ -14,4 +14,13 @@ print("Unique:",np.unique(blastp[0]).shape,np.unique(kofamscan[0]).shape)
 print(blastp)
 print(kofamscan)
 
-isolate=pd.read_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases")
+isolate=pd.read_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/5_isolate_step_1_kinases_02_predictions_full.csv")
+print(len(isolate))
+
+print(np.sum(isolate["seq_id"].str.contains("IBODOACL_")),np.sum(isolate["seq_id"].str.contains("IBODOACJ_")))
+
+blastp_contained=blastp.isin(isolate["seq_id"])
+kofamscan_contained=kofamscan.isin(isolate["seq_id"])
+
+print("BLASTP and 5_isolate:",np.sum(blastp_contained))
+print("KofamScan and 5_isolate:",np.sum(kofamscan_contained))
