bf8e602b9d73b9567bcd9217fb852346fe9a1f12
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-22 21:40:57 -0700

    Update check_step_3_11_family.py

---
+++ data_splits/check_step_3_11_family.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import pandas as pd
+
+dataset_path="../../kinases_dataset/step_3_11_family/protein/multi_class"
+
+train_path=os.path.join(dataset_path,"train/train.csv")
+dev_path=os.path.join(dataset_path,"dev/dev.csv")
+test_path=os.path.join(dataset_path,"test/test.csv")
+
+train=pd.read_csv(train_path)
+dev=pd.read_csv(dev_path)
+test=pd.read_csv(test_path)
+
+print(train)
+print(dev)
+print(test)
+print(len(train)+len(dev)+len(test))
+
+dfs={"train":train,"dev":dev,"test":test}
+for df in dfs:
+  print(df)
+  chosen_df=dfs[df]
+  print("Unique labels:",np.unique(chosen_df["label"]).shape)
+  labels=np.unique(chosen_df["label"])
+  for label in labels:
+    print(f"Count for {label}:",np.sum(chosen_df["label"]==label))
+  print()
