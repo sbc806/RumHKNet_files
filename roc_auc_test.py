54beab85554a13633ce124927c01fcbeba44c026
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-24 00:05:44 -0700

    Update roc_auc_test.py

---
+++ roc_auc_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.metrics import average_precision_score, roc_auc_score
+y_true = np.array([0, 0, 1, 1])
+y_scores = np.array([0.1, 0.4, 0.35, 0.8])
+average_precision_score(y_true, y_scores)
+y_true = np.array([0, 0, 1, 1, 2, 2])
+y_scores = np.array([
+    [0.7, 0.2, 0.05,0.05],
+    [0.4, 0.3, 0.15,0.15],
+    [0.1, 0.8, 0.05,0.05],
+    [0.2, 0.3, 0.25,0.25],
+    [0.4, 0.4, 0.1,0.1],
+    [0.1, 0.2, 0.35,0.35],
+])
+print(average_precision_score(y_true, y_scores))
+
+print(roc_auc_score(y_true, y_scores))
+
