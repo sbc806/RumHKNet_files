963f7330b92b8691036faa7e541b45f7a6627017
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-25 21:39:43 -0700

    Update predictions_helpers.py

--- predictions_files/predictions_helpers.py
+++ predictions_files/predictions_helpers.py
@@ -144,6 +144,7 @@ def check_specific(dir_path,f_name):
   dfs=[]
   for f in selected_files:
     df=pd.read_csv(os.path.join(dir_path,f))
+    assert len(df.columns)==4
     df.columns=["seq_id","seq","prob","pred"]
     # print(df.columns)
     # print(f,len(df))
