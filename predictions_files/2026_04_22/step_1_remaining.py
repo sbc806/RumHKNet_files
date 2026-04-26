b05f99703697ad056a7f0ef834b0f4a24deaeea9
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-25 21:52:42 -0700

    Update step_1_remaining.py

---
+++ predictions_files/2026_04_22/step_1_remaining.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append("..")
+from predictions_helpers import check_specific
+
+dataset_path= "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
+
+i_df={}
+small=[]
+small_total=0
+for i in range(0,5):
+  df=pd.read_csv(os.path.join(dataset_path,f"2026_04_22_clustered95_rep_seq_small_{i}.csv")
+  df_selected=df.iloc[800000:]
+  print(df_selected)
