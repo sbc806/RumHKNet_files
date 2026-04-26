205482adc68a289b00e303c2d3696bef0ab9a741
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-25 21:43:40 -0700

    Update step_1_results.py

--- predictions_files/2026_04_22/step_1_results.py
+++ predictions_files/2026_04_22/step_1_results.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
 import pandas as pd
+import sys
+sys.path.append("..")
+from predictions_helpers import check_specific
 
 dataset_path= "/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
@@ -13,3 +16,6 @@ for i in range(0,7):
   print(i,len(df))
   small_total=small_total+len(df)
 print(small_total)
+
+large=pd.read_csv(os.path.join(predictions_path,"2026_04_22_clustered95_rep_seq_large_predicted_02_v2.csv"))
+print("Number of predictions for sequences with length >1500:",len(large))
