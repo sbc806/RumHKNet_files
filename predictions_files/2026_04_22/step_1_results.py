9dc55ad2ce36dc5023454d06e3dbcb585505dfe2
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-26 22:34:12 -0700

    Update step_1_results.py

--- predictions_files/2026_04_22/step_1_results.py
+++ predictions_files/2026_04_22/step_1_results.py
@@ -45,8 +45,10 @@ print(small_all_kinase)
 # small_all_kinase.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/2026_04_22_clustered95_rep_seq_step_1_kinase_small.csv",index=False)
 
 small_remaining_all_1=pd.concat(small_remaining)
-small_kinase_remaining_1=small_remaining_1["prob"]>=threshold
+small_kinase_remaining_1=small_remaining_all_1["prob"]>=threshold
 print(f"Number of predicted kinases with threshold {threshold}:",np.sum(small_kinase_remaining_1),np.sum([small_remaining_all_1["pred"]==1))
 small_remaining_all_kinase_1=small_remaining_all_1[small_kinase_remaining_1][["seq_id","seq"]]
+print(small_remaining_all_kinase_1)
+
 # large=pd.read_csv(os.path.join(predictions_path,"2026_04_22_clustered95_rep_seq_large_predicted_02_v2.csv"))
 # print("Number of predictions for sequences with length >1500:",len(large))
