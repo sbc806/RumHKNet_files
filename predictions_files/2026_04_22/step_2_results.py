0a474b834bc6d9736401ab068d67f693728ceffc
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-26 22:45:10 -0700

    Update step_2_results.py

---
+++ predictions_files/2026_04_22/step_2_results.py
@@ -0,0 +1,10 @@
+import os as os
+import pandas as pd
+import numpy as np
+import sys
+sys.append("..")
+from predictions_helpers import check_specific
+
+dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
+predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_2/both/clustered"
+small_1=check_specific(predictions_path,"2026_04_22_clustered95_rep_seq_step_1_kinase_small_predicted")
