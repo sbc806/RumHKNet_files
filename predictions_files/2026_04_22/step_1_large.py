99c3b7de7cfb8a026c1a48486104c90e95f70407
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-27 00:41:29 -0700

    Update step_1_large.py

--- predictions_files/2026_04_22/step_1_large.py
+++ predictions_files/2026_04_22/step_1_large.py
@@ -15,5 +15,6 @@ less_equal_10000=large_argsort["seq"].str.len()<=10000
 
 large_argsort_1=large_argsort[less_equal_10000]
 large_argsort_2=large_argsort[~less_equal_10000]
+print(len(large_argsort_1)+len(large_argsort_2),len(large_argsort_1),len(large_argsort_2))
 print(large_argsort_1)
 print(large_argsort_2)
