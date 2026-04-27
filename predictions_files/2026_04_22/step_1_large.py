c143ab44ae455e24bfe7f6fa51cf3fd01c1c1c3d
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-27 00:36:19 -0700

    Update step_1_large.py

--- predictions_files/2026_04_22/step_1_large.py
+++ predictions_files/2026_04_22/step_1_large.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
+
+large=pd.read_csv(os.path.join(dataset_path,"2026_04_22_clustered95_rep_seq_large.csv"))
+print(large)
