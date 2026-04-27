ec125e5a6e3ca64ab4f4fa990a58f28e9f3368bd
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-26 22:47:42 -0700

    Update step_2_results.py

--- predictions_files/2026_04_22/step_2_results.py
+++ predictions_files/2026_04_22/step_2_results.py
@@ -10,6 +10,7 @@ predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predicti
 
 
 small_1=check_specific(predictions_path,"2026_04_22_clustered95_rep_seq_step_1_kinase_small_predicted")
+print(len(small_1))
 threshold=0.2
 small_1_kinase=small_1["prob"]>=0.2
 print(f"Threshold {threshold}:",np.sum(small_1_kinase),np.sum(small_1["pred"]==1))
