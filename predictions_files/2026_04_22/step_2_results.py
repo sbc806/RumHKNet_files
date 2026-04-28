f8efc764f76cd67fc5ec754041ba225bac7445d5
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-27 22:32:58 -0700

    Update step_2_results.py

--- predictions_files/2026_04_22/step_2_results.py
+++ predictions_files/2026_04_22/step_2_results.py
@@ -25,3 +25,7 @@ print(f"Threshold {threshold}:",np.sum(small_0_1_kinase),np.sum(small_0_1["pred"
 small_0_1_kinase_chosen=small_0_1[small_0_1_kinase][["seq_id","seq"]]
 print(small_0_1_kinase_chosen)
 # small_0_1_kinase_chosen.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered/2026_04_22_clustered95_rep_seq_step_2_histidine_kinase_small_0_1.csv",index=False)
+
+large_1=pd.read_csv(os.path.join(predictions_path,"2026_04_22_clustered95_rep_seq_step_1_kinase_large_1_predicted_02_v2.csv"))
+print(len(large_1))
+print(np.sum(large_1["prob"]>=threshold),np.sum(large_1["pred"]==1))
