ad597a797bcbdba5897088021305e29c5a833058
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-18 13:40:26 -0700

    Update split_fasta_4824.py

--- predictions_files/2026_05_18/split_fasta_4824.py
+++ predictions_files/2026_05_18/split_fasta_4824.py
@@ -40,4 +40,4 @@ def split_chunks(df,chunk_size,save_path,save_name):
   print(max(df_small["seq"].str.len()),max(df_large["seq"].str.len()))
   
 chunk_size=1200000
-# split_chunks(seq_data_df,chunk_size,dataset_path,"4824human_newrun")
+split_chunks(seq_data_df,chunk_size,dataset_path,"4824human_newrun")
