f0936359720f02894724e09adf6d25a75d0b72db
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-21 19:29:57 -0700

    Update step_1_results_9342.py

--- predictions_files/2026_05_18/step_1_results_9342.py
+++ predictions_files/2026_05_18/step_1_results_9342.py
@@ -8,15 +8,15 @@ from predictions_helpers import check_specific, df_to_fasta
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_1/both/clustered"
 
-small_dfs=[]
-for i in range(0,3):
-  df_i=check_specific(predictions_path,f"4824human_newrun_small_{i}_predicted")
+small_1_dfs=[]
+for i in range(0,4):
+  df_i=check_specific(predictions_path,f"93424_all_proteins_newrun_1_small_{i}_predicted")
   print(i, len(df_i))
   small_dfs.append(df_i)
 small_df=pd.concat(small_dfs)
 
 threshold=0.2
-print(f"Number of predictions for threshold {threshold}:",np.sum(small_df["prob"]>=threshold),np.sum(small_df["pred"]==1))
+print(f"Number of predictions for threshold {threshold}:",np.sum(small_1_df["prob"]>=threshold),np.sum(small_1_df["pred"]==1))
 
 large_1_df=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_1_large_predicted_02_v2.csv"))
 print(f"Number of predictions for threshold {threshold}:",np.sum(large_1_df["prob"]>=threshold),np.sum(large_1_df["label"]))
