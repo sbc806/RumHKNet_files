e2e11b574bc5d474d386d8fb3cb37d1aec405e2c
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 20:55:55 -0700

    Update step_2_results_9342.py

--- predictions_files/2026_05_18/step_2_results_9342.py
+++ predictions_files/2026_05_18/step_2_results_9342.py
@@ -8,10 +8,10 @@ from predictions_helpers import check_specific, df_to_fasta
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_2/both/clustered"
 
-small_dfs_034=[]
+small_dfs_1_0123456_2_034=[]
 for i in range(0,2):
   df_i=check_specific(predictions_path,f"9342_all_proteins_newrun_step_1_kinase_small_{i}_predicted")
   small_dfs_034.append(df_i)
-small_df_034=pd.concat(small_dfs_034)
+small_df_034=pd.concat(small_dfs_1_0123456_2_034)
 print(len(small_df_034))
 print(np.sum(small_df_034["prob"]>=0.2),np.sum(small_df_034["pred"]==1))
