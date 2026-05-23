8f51060664dfae0040903b7599fd0686dc40fdc0
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 20:56:35 -0700

    Update step_2_results_9342.py

--- predictions_files/2026_05_18/step_2_results_9342.py
+++ predictions_files/2026_05_18/step_2_results_9342.py
@@ -11,7 +11,7 @@ predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predicti
 small_dfs_1_0123456_2_034=[]
 for i in range(0,2):
   df_i=check_specific(predictions_path,f"9342_all_proteins_newrun_step_1_kinase_small_{i}_predicted")
-  small_dfs_034.append(df_i)
-small_df_034=pd.concat(small_dfs_1_0123456_2_034)
-print(len(small_df_034))
-print(np.sum(small_df_034["prob"]>=0.2),np.sum(small_df_034["pred"]==1))
+  small_dfs_1_0123456_2_034.append(df_i)
+small_df_1_0123456_2_034=pd.concat(small_dfs_1_0123456_2_034)
+print(len(small_df_1_0123456_2_034))
+print(np.sum(small_df_1_0123456_2_034["prob"]>=0.2),np.sum(small_df_1_0123456_2_034["pred"]==1))
