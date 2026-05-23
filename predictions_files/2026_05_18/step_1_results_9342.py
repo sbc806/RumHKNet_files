440a7548b1006cdee5665c0eae3c78658989730a
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 21:31:27 -0700

    Update step_1_results_9342.py

--- predictions_files/2026_05_18/step_1_results_9342.py
+++ predictions_files/2026_05_18/step_1_results_9342.py
@@ -36,24 +36,32 @@ print(small_kinase_df)
 # small_kinase_df.iloc[400000:].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_newrun_step_1_kinase_small_1.csv",index=False)
 print()
 
-small_kinase_df_2_5=check_specific(predictions_path,f"9342_all_proteins_newrun_2_small_5_predicted")
-print(5,len(small_kinase_df_2_5))
-print(np.sum(small_kinase_df_2_5["prob"]>=0.2),np.sum(small_kinase_df_2_5["pred"]==1))
+small_df_2_5=check_specific(predictions_path,f"9342_all_proteins_newrun_2_small_5_predicted")
+print(5,len(small_df_2_5))
+print(np.sum(small_df_2_5["prob"]>=0.2),np.sum(small_df_2_5["pred"]==1))
+small_kinase_df_2_5=small_df_2_5[small_df_2_5["pred"]==1]
+print(small_kinase_df_2_5)
+small_kinase_df_2_5.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_newrun_step_1_kinase_small_2_5.csv",index=False)
 print()
 
 large_1_df=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_1_large_sorted_predicted_02_v2.csv"))
 print(len(large_1_df))
 print(f"Number of predictions for threshold {threshold}:",np.sum(large_1_df["prob"]>=threshold),np.sum(large_1_df["label"]))
 
-large_1_histidine_kinase_df=large_1_df[large_1_df["label"]==1]
-print(max(large_1_histidine_kinase_df["seq"].str.len()))
+large_1_kinase_df=large_1_df[large_1_df["label"]==1]
+print(max(large_1_kinase_df["seq"].str.len()))
 
 large_2_df=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_2_large_sorted_predicted_02_v2.csv"))
 print(len(large_2_df))
 print(f"Number of predictions for threshold {threshold}:",np.sum(large_2_df["prob"]>=threshold),np.sum(large_2_df["label"]))
 
-large_2_histidine_kinase_df=large_2_df[large_2_df["label"]==1]
-print(max(large_2_histidine_kinase_df["seq"].str.len()))
+large_2_kinase_df=large_2_df[large_2_df["label"]==1]
+print(max(large_2_kinase_df["seq"].str.len()))
+print()
+
+large_kinase_df=pd.concat([large_kinase_df,large_2_kinase_df])[["seq_id","seq"]]
+print(large_kinase_df)
+large_kinase_df.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_1/clustered/9342_all_proteins_newrun_step_1_kinase_large.csv",index=False)
 
 """
 complete_df=pd.concat([small_df,large_df])
