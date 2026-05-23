553f009c0718d10fa04cb1a9e52a916723d20f21
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 20:15:33 -0700

    Update step_2_results_4824.py

--- predictions_files/2026_05_18/step_2_results_4824.py
+++ predictions_files/2026_05_18/step_2_results_4824.py
@@ -9,7 +9,11 @@ from predictions_helpers import check_specific, df_to_fasta
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_2/both/clustered"
 
+small_df=check_specific(predictions_path,"4824human_newrun_step_1_kinase_small_predicted")
+print(len(small_df))
+print(np.sum(small_df["prob"]>=0.2),np.sum(small_df["pred"]==1))
+
 large_df=pd.read_csv(os.path.join(predictions_path,"4824human_newrun_step_1_kinase_large_predicted_02_v2.csv"))
 
 print(len(large_df))
-print(np.sum(large_df["prob"]>=0.2),np.sum(large_df["label"]))
+print(np.sum(large_df["prob"]>=0.2),np.sum(large_df["label"]==1))
