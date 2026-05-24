3c9b2f8fec369722b6080b5fe10340a47e20cb7f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-24 01:15:08 -0700

    Update step_3_results_9342.py

--- predictions_files/2026_05_18/step_3_results_9342.py
+++ predictions_files/2026_05_18/step_3_results_9342.py
@@ -23,7 +23,7 @@ def adjusted_df(df):
   return df_selected
   
 all_predictions_selected=adjusted_df(all_predictions)
-# all_predictions_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered/4824human_newrun_step_3_histidine_kinase_family.csv",index=False)
+all_predictions_selected.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered/9342_all_proteins_newrun_step_3_histidine_kinase_family.csv",index=False)
 
 """
 complete=pd.concat([small,small_0_1,small_2_3,large_1])
