71c6972951cbc5c9ce4f2b1613cc09598ea9b154
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-24 01:04:23 -0700

    Update step_3_results_4824.py

--- predictions_files/2026_05_18/step_3_results_4824.py
+++ predictions_files/2026_05_18/step_3_results_4824.py
@@ -9,6 +9,6 @@ from predictions_helpers import check_specific, df_to_fasta
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_3/both/clustered"
 
-all_predictions=pd.read_csv(os.path.join(predictions_path,"4842human_newrun_step_2_histidine_kinase_predicted_02.csv"))
+all_predictions=pd.read_csv(os.path.join(predictions_path,"4824human_newrun_step_2_histidine_kinase_predicted_02.csv"))
 print("Number of predictions:",len(all_predictions))
 print(all_predictions.columns)
