4873ca6e3513360c02db6070fd6baacacdf16d52
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-24 14:27:21 -0700

    Update step_2_results_9342.py

--- predictions_files/2026_05_18/step_2_results_9342.py
+++ predictions_files/2026_05_18/step_2_results_9342.py
@@ -20,9 +20,9 @@ print(small_histidine_kinase_df_1_0123456_2_034)
 # small_histidine_kinase_df_1_0123456_2_034.to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_3/clustered/9342_all_proteins_newrun_step_2_histidine_kinase_small_1_0123456_2_034.csv",index=False)
 print()
 
-small_df_2_12=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_step_1_kinse_small_2_12_predicted_02_v2.csv"))
+small_df_2_12=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_step_1_kinase_small_2_12_predicted_02_v2.csv"))
 print(len(small_df_2_12))
-print(np.sum(small_df_2_12["prob"]>=0.2),np.sum(small-df_2_12["plabel"]==1))
+print(np.sum(small_df_2_12["prob"]>=0.2),np.sum(small_df_2_12["plabel"]==1))
 
 print()
 
@@ -31,3 +31,7 @@ print(len(small_df_2_5))
 print(np.sum(small_df_2_5["prob"]>=0.2),np.sum(small_df_2_5["label"]==1))
 
 print()
+
+large_df_1_2=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_step_1_kinase_large_1_2_predicted_02_v2.csv"))
+print(len(large_df_1_2))
+print(np.sum(large_df_1_2["prob"]>=0.2),np.sum(large_df_1_2["label"]==1))
