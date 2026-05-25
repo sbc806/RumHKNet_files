20a38da79f847f850e15cb2712631c4723602d29
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-25 15:00:06 -0700

    Update step_4_results_4824.py

--- predictions_files/2026_05_18/step_4_results_4824.py
+++ predictions_files/2026_05_18/step_4_results_4824.py
@@ -30,6 +30,6 @@ complete_selected=add_label(complete_selected,reverse_dict(ko_label))
 print(complete_selected)
 
 save_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/2026_05_18_cluster_data/RumHKNet_predictions/4824human_newrun/step_1_02_step_2_02"
-save_name="2026_04_22_clustered95_rep_seq_step_4_histidine_kinase_ko"
+save_name="4824human_newrun_step_4_histidine_kinase_ko"
 complete_selected.to_csv(os.path.join(save_path,f"{save_name}.csv"),index=False)
 df_to_fasta(complete_selected,os.path.join(save_path,f"{save_name}.fasta"),extra_column="pred_other")
