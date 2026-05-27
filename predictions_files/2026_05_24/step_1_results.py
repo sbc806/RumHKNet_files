d853bc88bf9f675796ae2becc59b8df0d5ecdc48
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-27 15:53:25 -0700

    Update step_1_results.py

--- predictions_files/2026_05_24/step_1_results.py
+++ predictions_files/2026_05_24/step_1_results.py
@@ -26,7 +26,7 @@ print(len(small_3))
 print(np.sum(small_3["prob"]>=0.2),np.sum(small_3["pred"]==1))
 small_3_kinase=small_3[small_3["pred"]==1]
 print(len(small_3_kinase))
-small_3_kinase[["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_remove2_step_1_kinase_small_3.csv",index=False)
+# small_3_kinase[["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_remove2_step_1_kinase_small_3.csv",index=False)
 print()
 
 large=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_remove2_large_sorted_predicted_02_v2.csv"))
@@ -35,4 +35,4 @@ print(np.sum(large["prob"]>=0.2),np.sum(large["label"]==1))
 large_kinase=large[large["label"]==1]
 print(len(large_kinase),max(large_kinase["seq"].str.len()))
 
-large_kinase[["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_remove2_step_1_kinase_large.csv",index=False)
+# large_kinase[["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_remove2_step_1_kinase_large.csv",index=False)
