75ccde77a5a6dcf2bf3168160d05efde2e7876fa
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-28 11:35:46 -0700

    Update step_2_results.py

--- predictions_files/2026_05_24/step_2_results.py
+++ predictions_files/2026_05_24/step_2_results.py
@@ -31,7 +31,6 @@ print(len(small_3))
 print(np.sum(small_3["prob"]>=0.2),np.sum(small_3["label"]==1))
 small_3_histidine_kinase=small_3[small_3["label"]==1]
 print(len(small_3_histidine_kinase))
-# small_3_kinase[["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_remove2_step_1_kinase_small_3.csv",index=False)
 print()
 
 large=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_remove2_step_1_kinase_large_predicted_02_v2.csv"))
@@ -39,8 +38,6 @@ print(len(large))
 print(np.sum(large["prob"]>=0.2),np.sum(large["label"]==1))
 large_histidine_kinase=large[large["label"]==1]
 print(len(large_histidine_kinase),max(large_histidine_kinase["seq"].str.len()))
-
-# large_kinase[["seq_id","seq"]].to_csv("/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered/9342_all_proteins_remove2_step_1_kinase_large.csv",index=False)
 print()
 
 small_3_large_histidine_kinase=pd.concat([small_3_histidine_kinase,large_histidine_kinase])
@@ -62,7 +59,7 @@ print(small_3.columns)
 print(large.columns)
 complete=pd.concat([small_012,small_3,large])
 print(len(complete))
-print(np.sum(complete["pred"]>=1),len(small_012),len(small_3_histidine),len(large_histidine),len(small_012_histidine)+len(small_3_histidine)+len(large_histidine))
+print(np.sum(complete["pred"]>=1),len(small_012_histidine_kinase),len(small_3_histidine_kinase),len(large_histidine_kinase),len(small_012_histidine_kinase)+len(small_3_histidine_kinase)+len(large_histidine_kinase))
 complete_histidine_kinase=pd.concat([small_012_histidine_kinase,small_3_histidine_kinase,large_histidine_kinase])
 print(len(complete_histidine_kinase))
 
