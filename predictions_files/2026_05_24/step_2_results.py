519752127152f4a3497525b4050916923f890982
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-28 11:23:00 -0700

    Update step_2_results.py

--- predictions_files/2026_05_24/step_2_results.py
+++ predictions_files/2026_05_24/step_2_results.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import sys
 sys.path.append("..")
-from predictions_helpers import check_specific
+from predictions_helpers import check_specific, df_to_fasta
 
 
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_2/clustered"
@@ -53,3 +53,18 @@ print(large.columns)
 complete=pd.concat([small_012,small_3,large])
 print(len(complete),np.unique(complete["seq_id"]).shape,np.unique(complete["seq"]).shape)
 """
+print()
+
+print(small_012.columns)
+small_3.columns=small_012.columns
+large.columns=small_012.columns
+print(small_3.columns)
+print(large.columns)
+complete=pd.concat([small_012,small_3,large])
+print(np.sum(complete["pred"]>=1),len(small_012),len(small_3),len(large),len(small_012)+len(small_3)+len(large))
+complete_histidine_kinase=pd.concat([small_012_histidine_kinase,small_3_histidine_kinase,large_histidine_kinase])
+print(len(complete_histidine_kinase))
+
+save_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/step_1_02_step_2_02"
+complete.to_csv(os.path.join(save_path,"9342_all_proteins_remove2_step_2_predictions_02.csv"),index=False)
+df_to_fasta(complete_histidine_kinase,os.path.join(save_path,"9342_all_proteins_remove2_step_2_histidine_kinase_02.fasta"))
