d493deb703adaf44dc53f85ad8fee8aa93c80172
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-28 20:21:30 -0700

    Update step_4_results.py

--- predictions_files/2026_05_24/step_4_results.py
+++ predictions_files/2026_05_24/step_4_results.py
@@ -10,7 +10,7 @@ from predictions_helpers import check_specific, df_to_fasta, add_label, reverse_
 dataset_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predictions_dataset/step_4/clustered"
 predictions_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases/predictions/predicted_results/step_4/both/clustered"
 
-small_3_large=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_remove2_step_3_histidine_kinase_family_small_3_large_predicted_02_v2.csv"))
+small_3_large=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_remove2_step_3_histidine_kinase_family_small_3_large_predicted_02_v2def.csv"))
 print("Number of predictions:",len(small_3_large))
 print(small_3_large.columns)
 # all_predictions_remaining=pd.read_csv(os.path.join(predictions_path,"9342_all_proteins_newrun_step_3_histidine_kinase_family_remaining_predicted_02_v2.csv"))
