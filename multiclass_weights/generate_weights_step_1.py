0f93fc17ddea45c5e5397b7636847750a844b810
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-07-13 23:28:38 -0700

    Update generate_weights_step_1.py

--- multiclass_weights/generate_weights_step_1.py
+++ multiclass_weights/generate_weights_step_1.py
@@ -5,7 +5,7 @@ import pandas as pd
 train_multiclass = pd.read_csv("/home/schen123/scratch/kinases/kinases_dataset/step_1_non_kinases_preprocessed/protein/binary_class/train/train.csv")
 print("Number of counts per label:", train_multiclass["label"].value_counts())
 print(min(train_multiclass["label"].value_counts())/max(train_multiclass["label"].value_counts()))
-print(train_multiclass["label"].value_counts[1]/len(train_multiclass)))
+print(train_multiclass["label"].value_counts()[1]/len(train_multiclass)))
 """
 for i in range(0,133):
     total=len(train_multiclass)
