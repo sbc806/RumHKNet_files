17f99aed45ceb21f689fb16313871f5a621fe4ba
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-07-13 23:18:34 -0700

    Update generate_weights_step_1.py

---
+++ multiclass_weights/generate_weights_step_1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+train_multiclass = pd.read_csv("/home/schen123/scratch/schen123/kinases/kinases_dataset/step_1_non_kinases_preprocessed/train/train.csv")
+print("Number of counts per label:", train_multiclass["label"].value_counts())
+"""
+for i in range(0,133):
+    total=len(train_multiclass)
+    count=train_multiclass["label"].value_counts()[i]
+    print(i,total,count)
+    new_weight=None
+    if np.floor(total/133/count) == 0:
+        new_weight=1/5
+    print(total/133/0.2/count,np.floor(total/133/0.2/count),total/133/count,np.floor(total/133/count),new_weight,total/(count*133))
+    print()
+  """
