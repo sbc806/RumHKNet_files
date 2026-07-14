c3e1ef7ef33781d1200e42ca165d364088172a83
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-07-13 23:54:28 -0700

    Update generate_weights_step_2.py

--- multiclass_weights/generate_weights_step_2.py
+++ multiclass_weights/generate_weights_step_2.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
+import os
 
+dir_path="/home/schen123/scratch/kinases/kinases_dataset/extra_p_2_class_v3_kinases_only/protein/binary_class"
+train_multiclass = pd.read_csv(os.path.join(dir_path,"train/train.csv"))
+dev=pd.read_csv(os.path.join(dir_path,"dev/dev.csv"))
+test=pd.read_csv(os.path.join(dir_path,"test/test.csv"))
+full=pd.concat([train_multiclass,dev,test])
+print(full)
+print(len(full),np.sum(full["label"]==0),np.sum(full["label"]==1))
 
-train_multiclass = pd.read_csv("/home/schen123/scratch/kinases/kinases_dataset/extra_p_2_class_v3_kinases_only/protein/binary_class/train/train.csv")
 neg=np.sum(train_multiclass["label"]==0)
 pos=np.sum(train_multiclass["label"]==1)
 print("Number of counts per label:", train_multiclass["label"].value_counts())
